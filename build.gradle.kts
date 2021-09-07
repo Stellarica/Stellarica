4ba861fab832f155907190086622d2e31797976d
Astralchroma <astralchroma@proton.me>
2021-09-07 19:42:45 +0100

    Tab

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
-    kotlin("jvm") version "1.5.21"
+	kotlin("jvm") version "1.5.21"
 
-    id("com.github.johnrengelman.shadow") version "7.0.0"
+	id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 repositories {
-    maven { url = uri("https://papermc.io/repo/repository/maven-public/") }
+	maven { url = uri("https://papermc.io/repo/repository/maven-public/") }
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
+	compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
 }
