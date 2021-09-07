3a04def18fa6ea71d3be8ae47be243445572e6c9
Peter Crawley <peterzcrawley@gmail.com>
2021-09-07 20:05:47 +0100

    Right click detection

---
+++ src/main/kotlin/Interface.kt
@@ -0,0 +1,24 @@
+package io.github.petercrawley.minecraftstarshipplugin
+
+import org.bukkit.Material
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.EquipmentSlot
+
+class Interface: Listener {
+	@EventHandler
+	fun interfaceUse(event: PlayerInteractEvent) {
+		if (event.hand != EquipmentSlot.HAND) return // PlayerInteractEvent is called twice for each hand.
+		if (event.action != Action.RIGHT_CLICK_BLOCK) return // Ignore Air Punchers
+
+		// We know that event.clickedBlock is not null at this point because of the action.
+		if (event.clickedBlock!!.type != Material.JUKEBOX) return // Ignore blocks we don't care about.
+
+		// Later we will do more then just attempt to detect a ship
+		Starship(event.player).detect()
+
+		event.isCancelled = true
+	}
+}
