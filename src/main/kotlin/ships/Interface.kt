3af2dffadaadc6d1ecbe95a0e540df712a33e907
Peter Crawley <peterzcrawley@gmail.com>
2021-09-08 16:34:56 +0100

    Revert "Stop passing the plugin around"
    This reverts commit 91c72bb2d74534463cb6fc79426f5185a7b016d3.

--- src/main/kotlin/ships/Interface.kt
+++ src/main/kotlin/ships/Interface.kt
@@ -6,8 +6,9 @@ import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.EquipmentSlot
+import org.bukkit.plugin.java.JavaPlugin
 
-class Interface: Listener {
+class Interface(private val plugin: JavaPlugin): Listener {
 	@EventHandler
 	fun interfaceUse(event: PlayerInteractEvent) {
 		if (event.hand != EquipmentSlot.HAND) return // PlayerInteractEvent is called twice for each hand.
@@ -17,7 +18,7 @@ class Interface: Listener {
 		if (event.clickedBlock!!.type != Material.JUKEBOX) return // Ignore blocks we don't care about.
 
 		// Later we will do more then just attempt to detect a ship
-		Starship(event.clickedBlock!!.location, event.player).detect()
+		Starship(event.clickedBlock!!.location, event.player, plugin).detect()
 
 		event.isCancelled = true
 	}
--- src/main/kotlin/ships/Starship.kt
+++ src/main/kotlin/ships/Starship.kt
@@ -1,14 +1,14 @@
 package io.github.petercrawley.minecraftstarshipplugin.ships
 
-import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.entity.Player
+import org.bukkit.plugin.java.JavaPlugin
 import ships.DetectionTask
 
-class Starship(val origin: Location, val player: Player) {
+class Starship(val origin: Location, val player: Player, private val plugin: JavaPlugin) {
 	val detectedBlocks: MutableSet<Location> = mutableSetOf()
 
 	fun detect() {
-		DetectionTask(this).runTaskAsynchronously(Bukkit.getPluginManager().getPlugin("MinecraftStarshipPlugin")!!)
+		DetectionTask(this).runTaskAsynchronously(plugin)
 	}
 }
