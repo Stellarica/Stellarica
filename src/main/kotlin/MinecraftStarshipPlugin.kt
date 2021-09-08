5f94757ae8e62e161fc5d9b91f777c47dd0ed128
Astralchroma <astralchroma@proton.me>
2021-09-08 16:24:46 +0100

    Stop blocking bukkit.

--- src/main/kotlin/MinecraftStarshipPlugin.kt
+++ src/main/kotlin/MinecraftStarshipPlugin.kt
@@ -1,11 +1,11 @@
 package io.github.petercrawley.minecraftstarshipplugin
 
-import io.github.petercrawley.minecraftstarshipplugin.Ships.Interface
+import io.github.petercrawley.minecraftstarshipplugin.ships.Interface
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class MinecraftStarshipPlugin: JavaPlugin() {
 	override fun onEnable() {
-		Bukkit.getPluginManager().registerEvents(Interface(), this)
+		Bukkit.getPluginManager().registerEvents(Interface(this), this)
 	}
 }
