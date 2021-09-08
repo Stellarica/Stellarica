27f738eb7bb7422f7ba70ba8ffd476f57e5191dd
Astralchroma <astralchroma@proton.me>
2021-09-08 17:40:55 +0100

    Stop playing pass the parcel with the plugin.

--- src/main/kotlin/ships/Starship.kt
+++ src/main/kotlin/ships/Starship.kt
@@ -1,14 +1,14 @@
 package io.github.petercrawley.minecraftstarshipplugin.ships
 
+import io.github.petercrawley.minecraftstarshipplugin.MinecraftStarshipPlugin
 import org.bukkit.Location
 import org.bukkit.entity.Player
-import org.bukkit.plugin.java.JavaPlugin
 import ships.DetectionTask
 
-class Starship(val origin: Location, val player: Player, private val plugin: JavaPlugin) {
+class Starship(val origin: Location, val player: Player) {
 	val detectedBlocks: MutableSet<Location> = mutableSetOf()
 
 	fun detect() {
-		DetectionTask(this).runTaskAsynchronously(plugin)
+		DetectionTask(this).runTaskAsynchronously(MinecraftStarshipPlugin.getPlugin())
 	}
 }
