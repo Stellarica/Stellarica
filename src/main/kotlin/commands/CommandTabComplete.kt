99bca26347ff23c4b16d1ad5c635ca6641cb097a
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2021-09-09 10:27:10 -0600

    Add config reload and reset commands

---
+++ src/main/kotlin/commands/CommandTabComplete.kt
@@ -0,0 +1,30 @@
+package io.github.petercrawley.minecraftstarshipplugin.commands
+
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.command.TabCompleter
+
+class CommandTabComplete : TabCompleter {
+    override fun onTabComplete(sender: CommandSender, cmd: Command, alias: String, args: Array<String>): MutableList<String> {
+        val subcommands: MutableList<String> = ArrayList()
+        if (args.size == 1) {
+            // /msp <x>
+            if (sender.hasPermission("msp.config.reload")) {
+                subcommands.add("config")
+            }
+        }
+
+        if (args.size == 2) {
+            if (args[0].equals("config", ignoreCase = true)) {
+                // /msp config <x>
+                if (sender.hasPermission("msp.config.reload")) {
+                    subcommands.add("reload")
+                }
+                if (sender.hasPermission("msp.config.reset")){
+                    subcommands.add("reset")
+                }
+            }
+        }
+        return subcommands;
+    }
+}
