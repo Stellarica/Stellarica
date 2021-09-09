44de4e0862d91ab70639de41fd0a0b93c31b64d1
trainb0y <trainb0ydev@gmail.com>
2021-09-09 10:27:10 -0600

    Add config reload and reset commands

--- src/main/kotlin/MinecraftStarshipPlugin.kt
+++ src/main/kotlin/MinecraftStarshipPlugin.kt
@@ -1,5 +1,7 @@
 package io.github.petercrawley.minecraftstarshipplugin
 
+import io.github.petercrawley.minecraftstarshipplugin.commands.CommandTabComplete
+import io.github.petercrawley.minecraftstarshipplugin.commands.MSPCommands
 import io.github.petercrawley.minecraftstarshipplugin.ships.Interface
 import org.bukkit.Bukkit
 import org.bukkit.Material
@@ -15,16 +17,22 @@ class MinecraftStarshipPlugin: JavaPlugin() {
 		}
 	}
 
-	val nonDetectableBlocks: MutableSet<Material> = mutableSetOf()
+	var nonDetectableBlocks: MutableSet<Material> = mutableSetOf()
 
 	override fun onEnable() {
 		plugin = this
 		plugin.saveDefaultConfig() // Save the default config, doesn't overwrite existing
+		plugin.updateNonDetectableBlocks()
 
 		Bukkit.getPluginManager().registerEvents(Interface(), this)
 
+		plugin.getCommand("msp")!!.setExecutor(MSPCommands())
+		plugin.getCommand("msp")!!.tabCompleter = CommandTabComplete()
+	}
+
+	fun updateNonDetectableBlocks(){
 		// Get the non-detectable blocks from the config file
-		// TODO: Config reload command
+		nonDetectableBlocks = mutableSetOf(Material.AIR)
 		config.getStringList("non-detectable-blocks").forEach {
 			if (Material.getMaterial(it) == null){
 				logger.warning("No Material for $it! Make sure all non-detectable blocks are correctly named!")
