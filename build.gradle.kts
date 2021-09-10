f3f1030a5885228fe6bbbc0ee36bd19acf283dad
Peter Crawley <peterzcrawley@gmail.com>
2021-09-10 22:02:08 +0100

    hjson dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,12 @@ plugins {
 }
 
 repositories {
+	mavenCentral()
+
 	maven { url = uri("https://papermc.io/repo/repository/maven-public/") }
 }
 
 dependencies {
 	compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
+	compileOnly("org.hjson:hjson:3.0.0")
 }
