b991f23c1614a0d245d2b0412e8784b15a22645c
trainb0y <trainb0ydev@gmail.com>
2021-09-12 15:51:13 -0600

    Remove a semicolon

--- src/main/kotlin/projectiles/ParticleProjectile.kt
+++ src/main/kotlin/projectiles/ParticleProjectile.kt
@@ -24,7 +24,7 @@ class ParticleProjectile (val origin: Location, val beam: Boolean, val color: Co
             }
         }
         else {
-            ProjectileRunnable(this).runTaskTimer(getPlugin(), 1, 1);
+            ProjectileRunnable(this).runTaskTimer(getPlugin(), 1, 1)
         }
     }
 
