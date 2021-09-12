a1a67c04d81e982dd4c0710de8d83a3ce1e03257
trainb0y <trainb0ydev@gmail.com>
2021-09-12 16:22:19 -0600

    Over-comment

--- src/main/kotlin/projectiles/ParticleProjectile.kt
+++ src/main/kotlin/projectiles/ParticleProjectile.kt
@@ -9,30 +9,43 @@ import org.bukkit.Particle
 import org.bukkit.entity.LivingEntity
 import org.bukkit.util.Vector
 
-class ParticleProjectile (val origin: Location, val color: Color, val range: Int, val minRange: Double, val speed: Int, val damage: Double, val explosion: Float) {
-    private val particle = ParticleBuilder(Particle.REDSTONE).color(color).force(true).count(2)
+class ParticleProjectile (val origin: Location, private val color: Color, private val particleDensity: Int,
+                          val range: Int, private val minRange: Double, val speed: Int,
+                          private val damage: Double, private val explosion: Float) // Yikes that's a lot of arguments
+{
+    private val particle = ParticleBuilder(Particle.REDSTONE).color(color).force(true).count(particleDensity)
+    // ParticleBuilder that we can spawn later when we need to
 
     fun shootBeam() {
-        val loc = origin.clone()
-        val direction: Vector = origin.direction
+        // Simple beam mode, do it all on the same server tick
+        val loc = origin.clone() // Don't want to modify the origin, so we clone it
+        val direction: Vector = origin.direction // Main reason this uses Locations (even though they are slow) is for this
+        // Basically represent the amount we move every step
         direction.multiply(range)
         direction.normalize()
-        // Simple beam mode, do it all on the same server tick
-        for (i in 0..range) {
+
+        for (i in 0..range) { // Go tick at every location in the range, during this server tick
             loc.add(direction)
             if (!tick(loc)) break // if it returned false it hit something
         }
     }
 
     fun shootProjectile() {
+        // Start the Runnable that will handle projectile mode
         ProjectileRunnable(this).runTaskTimer(getPlugin(), 1, 1)
     }
 
     fun tick(loc: Location): Boolean{
-        particle.location(loc).spawn()
+        // A single particle spawn + check for damage/blocks/explosion
+        // Returns false if it hit something, otherwise true.
+        particle.location(loc).spawn() // actually spawn the particle
 
         for (e in loc.world.getNearbyEntities(loc, 1.0, 1.0, 1.0)) {
             if (e is LivingEntity && origin.distance(loc) > minRange) {
+                // minRange exists so the person shooting (if its a gun or something)
+                // or the ship shooting don't blow themselves to bits. Its not a great system,
+                // but it works. We can't just exclude the player that shot it because we don't know if it *is* a
+                // player who shot it.
                 e.damage(damage)
                 if (explosion > 0) loc.world.createExplosion(loc, explosion)
                 return false // damage one entity only
@@ -40,10 +53,9 @@ class ParticleProjectile (val origin: Location, val color: Color, val range: Int
         }
 
         if (loc.block.type != Material.AIR) {
-            if (explosion > 0) {
-                if (origin.distance(loc) > minRange) {// don't want it exploding where it was shot from, but don't want to spam distance() as it's costly.
-                    // distance() might return NaN if the distance is too high...
-                    loc.world.createExplosion(loc, explosion)
+            if (explosion > 0) { // Sure this could be one line but checking distance is costly and we should only do it if we have to
+                if (origin.distance(loc) > minRange) { // distance() might return NaN if the distance is too high...
+                    loc.world.createExplosion(loc, explosion) // As far as I can tell this is the main cause of lag when spamming these
                 }
             }
             return false
--- src/main/kotlin/projectiles/ProjectileRunnable.kt
+++ src/main/kotlin/projectiles/ProjectileRunnable.kt
@@ -5,15 +5,19 @@ import org.bukkit.util.Vector
 
 class ProjectileRunnable(private val projectileHandler: ParticleProjectile) : BukkitRunnable() {
     private var counter = 0
-    private var loc = projectileHandler.origin.clone()
+    private var loc = projectileHandler.origin.clone() // Don't want to modify the origin
     private val direction: Vector = projectileHandler.origin.direction.multiply(projectileHandler.range).normalize()
+    // The amount that we change the location by every step
 
     override fun run() {
         if (counter > projectileHandler.range) cancel()
+        // Counter is basically how far are we toward the max range.
+        // We want to cancel when we reach that or else... big lag and eventually errors
 
-        for (i in 0..projectileHandler.speed){
+        for (i in 0..projectileHandler.speed){ // We have to do the speed number of steps per tick, otherwise its incredibly slow
             loc.add(direction)
-            if (!projectileHandler.tick(loc)) {
+
+            if (!projectileHandler.tick(loc)) { // Spawn the particle and check for blocks/entities
                 // it will be false if it hit something, so don't do any more steps
                 cancel()
                 break
