2f86d96e2453459484008b534482770c50975d16
Peter-Crawley <peterzcrawley@gmail.com>
2021-09-12 13:03:59 +0100

    Better way of handling custom materials.

---
+++ src/main/kotlin/customblocks/MSPMaterial.kt
@@ -0,0 +1,67 @@
+package io.github.petercrawley.minecraftstarshipplugin.customblocks
+
+import org.bukkit.Material
+import org.bukkit.block.Block
+import org.bukkit.block.BlockFace
+import org.bukkit.block.data.MultipleFacing
+
+class MSPMaterial {
+	private val customBlocks = mutableMapOf(
+		Pair(0b000000, "INTERFACE")
+	)
+
+	private var material: Any?
+
+	init {
+		material = null
+	}
+
+	constructor(value: Any) {
+		set(value)
+	}
+
+	fun set(value: Any?) {
+		when (value) {
+			is Block -> {
+				if (value.type == Material.MUSHROOM_STEM) {
+					val block = value.blockData as MultipleFacing
+					var id = 0
+
+					if (block.hasFace(BlockFace.DOWN))  id += 32
+					if (block.hasFace(BlockFace.EAST))  id += 16
+					if (block.hasFace(BlockFace.NORTH)) id +=  8
+					if (block.hasFace(BlockFace.SOUTH)) id +=  4
+					if (block.hasFace(BlockFace.UP))    id +=  2
+					if (block.hasFace(BlockFace.WEST))  id +=  1
+
+					material = customBlocks.getOrDefault(id, "MUSHROOM_STEM")
+
+					if (material == "MUSHROOM_STEM") material = value.type
+
+				} else material = value.type
+			}
+			is Material -> material = value
+			is String -> material = Material.getMaterial(value) ?: if (customBlocks.values.contains(value)) value else null
+			else -> material = null
+		}
+	}
+
+	override fun hashCode(): Int {
+		return material?.hashCode() ?: 0
+	}
+
+	override fun toString(): String {
+		return material.toString()
+	}
+
+	override fun equals(other: Any?): Boolean {
+		if (this === other) return true
+		if (javaClass != other?.javaClass) return false
+
+		other as MSPMaterial
+
+		if (material != other.material) return false
+
+		return true
+	}
+}
