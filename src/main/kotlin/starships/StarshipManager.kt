1d47c0cc48a3c233cb8cb72ce41c39b65bf8a7ff
PeterCrawley1 <peterzcrawley@gmail.com>
2021-09-14 18:06:31 +0100

    Start of a single block moving task

--- src/main/kotlin/starships/StarshipManager.kt
+++ src/main/kotlin/starships/StarshipManager.kt
@@ -1,9 +1,23 @@
 package io.github.petercrawley.minecraftstarshipplugin.starships
 
+import io.github.petercrawley.minecraftstarshipplugin.MinecraftStarshipPlugin.Companion.getPlugin
+import org.bukkit.Bukkit
 import org.bukkit.block.Block
+import org.bukkit.block.data.BlockData
 import org.bukkit.entity.Player
 
 object StarshipManager {
+    val blockMoves = mutableMapOf<Block, BlockData>()
+
+    init {
+        Bukkit.getScheduler().runTaskTimer(getPlugin(), Runnable {
+            if (blockMoves.isEmpty()) return@Runnable
+
+            
+            it.key.setBlockData(it.value, false)
+        }, 1, 1)
+    }
+
     fun getStarshipAt(block: Block, requester: Player): Starship {
         return Starship(block, requester)
     }
