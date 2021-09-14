b5fc592299532ddf82e1afb74d087b1979d2fb72
Peter-Crawley <peterzcrawley@gmail.com>
2021-09-14 22:25:10 +0100

    Do not let TPS drop

--- src/main/kotlin/starships/StarshipManager.kt
+++ src/main/kotlin/starships/StarshipManager.kt
@@ -5,18 +5,29 @@ import org.bukkit.Bukkit
 import org.bukkit.block.Block
 import org.bukkit.block.data.BlockData
 import org.bukkit.entity.Player
+import java.util.concurrent.ConcurrentHashMap
 
 object StarshipManager {
-    val blockMoves = mutableMapOf<Block, BlockData>()
+    val blockMoves = ConcurrentHashMap<Block, BlockData>()
 
     init {
         Bukkit.getScheduler().runTaskTimer(getPlugin(), Runnable {
             if (blockMoves.isEmpty()) return@Runnable
 
+            val start = System.currentTimeMillis()
+
+            val targetTime = start + 45
+
             blockMoves.forEach {
+                if (System.currentTimeMillis() > targetTime) return@forEach
+
                 it.key.setBlockData(it.value, false)
                 blockMoves.remove(it.key)
             }
+
+            val timeTook = System.currentTimeMillis() - start
+
+            getPlugin().logger.info(timeTook.toString())
         }, 1, 1)
     }
 
