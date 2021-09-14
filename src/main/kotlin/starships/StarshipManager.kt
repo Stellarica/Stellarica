cd863fb6046b3ba1c265377b6e78dd6ad21d67b6
Astralchroma <astralchroma@proton.me>
2021-09-14 16:38:56 +0100

    Create StarshipManager.kt

---
+++ src/main/kotlin/starships/StarshipManager.kt
@@ -0,0 +1,15 @@
+package io.github.petercrawley.minecraftstarshipplugin.starships
+
+object StarshipManager {
+    fun getStarshipAt(block: Block): Starship {
+
+    }
+
+    fun startTasks() {
+
+    }
+
+    fun stopTasks() {
+
+    }
+}
