a60ebc5044db5ff52a8e816417a37a66d5c2c4ed
Peter-Crawley <peterzcrawley@gmail.com>
2021-09-17 16:23:27 +0100

    Starship Activation

---
+++ src/main/kotlin/starships/StarshipTick.kt
@@ -0,0 +1,11 @@
+package io.github.petercrawley.minecraftstarshipplugin.starships
+
+import org.bukkit.scheduler.BukkitRunnable
+
+class StarshipTick: BukkitRunnable() {
+	override fun run() {
+		val start = System.currentTimeMillis()
+
+		val targetTime = start + 50
+	}
+}
