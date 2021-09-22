6268c7cf1f794558bcc75cdba87c2dfebe90403b
Astralchroma <astralchroma@proton.me>
2021-09-22 16:42:23 +0100

    Update Kotlin and specify target jvm as Java 16

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	kotlin("jvm") version "1.5.21"
+	kotlin("jvm") version "1.5.31"
 
 	id("com.github.johnrengelman.shadow") version "7.0.0"
 }
@@ -13,4 +13,10 @@ repositories {
 dependencies {
 	compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
 	compileOnly("org.hjson:hjson:3.0.0")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+	kotlinOptions {
+		jvmTarget = "16"
+	}
 }
