3cb5fe98f2648221e7d035d026df3e5528feecba
Astralchroma <astralchroma@proton.me>
2021-09-22 18:36:23 +0100

    Create ChunkLocation and allow for casting BlockLocation to ChunkLocation

--- src/main/kotlin/utils/BlockLocation.kt
+++ src/main/kotlin/utils/BlockLocation.kt
@@ -4,4 +4,8 @@ import org.bukkit.block.Block
 
 data class BlockLocation(var x: Int, var y: Int, var z: Int) {
 	constructor(block: Block) : this(block.x, block.y, block.z)
+
+	fun toChunkLocation(): ChunkLocation {
+		return ChunkLocation(x shr 4, z shr 4)
+	}
 }
