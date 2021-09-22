2c62d0f420f58aa03ff50e6630607df0d7603812
Peter-Crawley <peterzcrawley@gmail.com>
2021-09-22 17:21:11 +0100

    Re-implement InterfaceScreen

---
+++ src/main/kotlin/starships/screens/InterfaceScreen.kt
@@ -0,0 +1,25 @@
+package io.github.petercrawley.minecraftstarshipplugin.starships.screens
+
+import io.github.petercrawley.minecraftstarshipplugin.MinecraftStarshipPlugin.Companion.itemWithName
+import io.github.petercrawley.minecraftstarshipplugin.starships.Screen
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.event.inventory.InventoryType
+
+class InterfaceScreen(player: Player, starship: Any): Screen() {
+	init {
+		createScreen(player, InventoryType.HOPPER, "Starship Interface")
+
+		screen.setItem(0, itemWithName(Material.MINECART, "Detect Starship"    , 128, 255, 128, bold = true))
+		screen.setItem(1, itemWithName(Material.COMPASS , "Pilot Starship"     , 128, 128, 255, bold = true))
+		screen.setItem(4, itemWithName(Material.BEDROCK , "Allow Undetectables", 255, 128, 128, bold = true))
+	}
+
+	override fun onScreenButtonClicked(slot: Int) {
+		when (slot) {
+			1, 4 -> {
+				closeScreen()
+			}
+		}
+	}
+}
