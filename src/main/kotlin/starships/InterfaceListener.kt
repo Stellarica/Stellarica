d56078f7b6f8832048243027924ab5c0afd99681
Peter-Crawley <peterzcrawley@gmail.com>
2021-09-22 17:29:42 +0100

    Re-implement InterfaceListener

---
+++ src/main/kotlin/starships/InterfaceListener.kt
@@ -0,0 +1,21 @@
+package io.github.petercrawley.minecraftstarshipplugin.starships
+
+import io.github.petercrawley.minecraftstarshipplugin.customblocks.MSPMaterial
+import io.github.petercrawley.minecraftstarshipplugin.starships.screens.InterfaceScreen
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.EquipmentSlot
+
+class InterfaceListener: Listener {
+	@EventHandler fun onPlayerInteractEvent(event: PlayerInteractEvent) {
+		if (event.hand == EquipmentSlot.HAND && event.action == Action.RIGHT_CLICK_BLOCK && !event.player.isSneaking) {
+			if (MSPMaterial(event.clickedBlock!!) == MSPMaterial("INTERFACE")) {
+				InterfaceScreen(event.player, null)
+
+				event.isCancelled = true
+			}
+		}
+	}
+}
