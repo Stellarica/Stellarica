bfc44fb272e18512e6334fdd97c314bf41be1514
Astralchroma <astralchroma@proton.me>
2021-09-23 22:13:20 +0100

    relative()

--- src/main/kotlin/utils/BlockLocation.kt
+++ src/main/kotlin/utils/BlockLocation.kt
@@ -5,7 +5,7 @@ import org.bukkit.block.Block
 data class BlockLocation(var x: Int, var y: Int, var z: Int) {
 	constructor(block: Block) : this(block.x, block.y, block.z)
 
-	fun toChunkLocation(): ChunkLocation {
-		return ChunkLocation(x shr 4, z shr 4)
+	fun relative(x: Int, y: Int, z: Int): BlockLocation {
+		return BlockLocation(this.x + x, this.y + y, this.z + z)
 	}
 }
