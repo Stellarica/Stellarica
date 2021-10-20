0978046e142984f0b5611a62f561e88e2be5383f
trainb0y <trainb0ydev@gmail.com>
2021-10-20 15:26:04 -0600

    Projectile launch event

---
+++ src/main/kotlin/events/ParticleProjectileLaunchEvent.kt
@@ -0,0 +1,17 @@
+package io.github.petercrawley.minecraftstarshipplugin.events
+import io.github.petercrawley.minecraftstarshipplugin.projectiles.ParticleProjectile
+import org.bukkit.Location
+import org.bukkit.entity.Entity
+import org.bukkit.event.Event
+import org.bukkit.event.HandlerList
+
+class ParticleProjectileLaunchEvent(val projectile: ParticleProjectile, val location: Location) : Event() {
+
+    override fun getHandlers(): HandlerList {
+        return handlerList
+    }
+
+    companion object {
+        val handlerList = HandlerList()
+    }
+}
