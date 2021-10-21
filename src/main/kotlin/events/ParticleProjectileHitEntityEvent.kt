b07c2acb4cb0e2ff343039e0626d4a247c3733c1
trainb0y <trainb0ydev@gmail.com>
2021-10-21 10:25:32 -0600

    Spaces bad

--- src/main/kotlin/events/ParticleProjectileHitEntityEvent.kt
+++ src/main/kotlin/events/ParticleProjectileHitEntityEvent.kt
@@ -1,4 +1,5 @@
 package io.github.petercrawley.minecraftstarshipplugin.events
+
 import io.github.petercrawley.minecraftstarshipplugin.projectiles.ParticleProjectile
 import org.bukkit.Location
 import org.bukkit.entity.Entity
@@ -7,11 +8,11 @@ import org.bukkit.event.HandlerList
 
 class ParticleProjectileHitEntityEvent(val projectile: ParticleProjectile, val location: Location, val entity: Entity) : Event() {
 
-    override fun getHandlers(): HandlerList {
-        return handlerList
-    }
+	override fun getHandlers(): HandlerList {
+		return handlerList
+	}
 
-    companion object {
-        val handlerList = HandlerList()
-    }
+	companion object {
+		val handlerList = HandlerList()
+	}
 }
--- src/main/kotlin/events/StarshipPilotEvent.kt
+++ src/main/kotlin/events/StarshipPilotEvent.kt
@@ -1,17 +1,18 @@
 package io.github.petercrawley.minecraftstarshipplugin.events
+
 import io.github.petercrawley.minecraftstarshipplugin.starships.Starship
 import org.bukkit.entity.Player
 import org.bukkit.event.Event
 import org.bukkit.event.HandlerList
 
 class StarshipPilotEvent(val starship: Starship, val player: Player) : Event() {
-    // TODO: actually call this
+	// TODO: actually call this
 
-    override fun getHandlers(): HandlerList {
-        return handlerList
-    }
+	override fun getHandlers(): HandlerList {
+		return handlerList
+	}
 
-    companion object {
-        val handlerList = HandlerList()
-    }
+	companion object {
+		val handlerList = HandlerList()
+	}
 }
