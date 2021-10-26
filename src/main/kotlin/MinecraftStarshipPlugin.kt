76b21d2736310326081bb3fcb73866a5f74afad3
Peter-Crawley <peterzcrawley@gmail.com>
2021-10-26 20:18:41 +0100

    Back to YAML and NamedItem class

--- src/main/kotlin/MinecraftStarshipPlugin.kt
+++ src/main/kotlin/MinecraftStarshipPlugin.kt
@@ -5,89 +5,26 @@ import io.github.petercrawley.minecraftstarshipplugin.commands.Commands
 import io.github.petercrawley.minecraftstarshipplugin.customblocks.CustomBlocksListener
 import io.github.petercrawley.minecraftstarshipplugin.customblocks.MSPMaterial
 import io.github.petercrawley.minecraftstarshipplugin.starships.InterfaceListener
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.Component.translatable
-import net.kyori.adventure.text.format.Style.style
-import net.kyori.adventure.text.format.TextColor.color
-import net.kyori.adventure.text.format.TextDecoration
 import org.bstats.bukkit.Metrics
 import org.bukkit.Bukkit
-import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
-import org.hjson.JsonObject
-import org.hjson.JsonValue
-import java.io.File
 
 class MinecraftStarshipPlugin : JavaPlugin() {
 	companion object {
 		lateinit var plugin: MinecraftStarshipPlugin
 			private set
 
-		fun itemWithName(
-			material: org.bukkit.Material,
-			name: String,
-			colorR: Int = 255,
-			colorG: Int = 255,
-			colorB: Int = 255,
-			bold: Boolean = false,
-			italic: Boolean = false
-		): ItemStack {
-			val item = ItemStack(material)
+		var timeOperations: Boolean = false
+			private set
 
-			val itemMeta = item.itemMeta
+		var detectionLimit: Int = 500000
+			private set
 
-			itemMeta.displayName(
-				Component.text(name).style(
-					style(color(colorR, colorG, colorB)).decoration(TextDecoration.ITALIC, italic)
-						.decoration(TextDecoration.BOLD, bold)
-				)
-			)
+		var forcedUndetectable = setOf<MSPMaterial>()
+			private set
 
-			item.itemMeta = itemMeta
-
-			return item
-		}
-
-		fun itemWithTranslatableName(
-			material: org.bukkit.Material,
-			name: String,
-			colorR: Int = 255,
-			colorG: Int = 255,
-			colorB: Int = 255,
-			bold: Boolean = false,
-			italic: Boolean = false
-		): ItemStack {
-			val item = ItemStack(material)
-
-			val itemMeta = item.itemMeta
-
-			itemMeta.displayName(
-				translatable(name).style(
-					style(
-						color(
-							colorR,
-							colorG,
-							colorB
-						)
-					).decoration(TextDecoration.ITALIC, italic).decoration(TextDecoration.BOLD, bold)
-				)
-			)
-
-			item.itemMeta = itemMeta
-
-			return item
-		}
-
-		var forcedUndetectable = mutableSetOf<MSPMaterial>()
-		var defaultUndetectable = mutableSetOf<MSPMaterial>()
-		var mainConfig: JsonObject = JsonObject()
-	}
-
-	private fun saveDefault(name: String) {
-		if (!File(plugin.dataFolder, name).exists()) {
-			// Although it won't overwrite it creates some useless warnings
-			plugin.saveResource(name, false)
-		}
+		var defaultUndetectable = setOf<MSPMaterial>()
+			private set
 	}
 
 	override fun onEnable() {
@@ -95,6 +32,7 @@ class MinecraftStarshipPlugin : JavaPlugin() {
 
 		Metrics(this, 12863)
 
+		saveDefaultConfig()
 		reloadConfig()
 
 		Bukkit.getPluginManager().registerEvents(InterfaceListener(), this)
@@ -105,29 +43,21 @@ class MinecraftStarshipPlugin : JavaPlugin() {
 	}
 
 	override fun reloadConfig() {
-		plugin.saveDefault("undetectables.hjson")
-		plugin.saveDefault("config.hjson")
+		super.reloadConfig()
 
-		// Get the non-detectable blocks from the config file
-		forcedUndetectable = mutableSetOf()
-		defaultUndetectable = mutableSetOf()
+		timeOperations = plugin.config.getBoolean("timeOperations", false)
+		detectionLimit = plugin.config.getInt("detectionLimit", 500000)
 
-		val configFile = JsonValue.readHjson(File(plugin.dataFolder, "undetectables.hjson").bufferedReader()).asObject()
-
-		configFile["forcedUndetectable"].asArray().forEach {
-			val value = MSPMaterial(it.asString())
-
-			if (value == null) logger.warning("No Material for $value! Make sure all forced undetectable blocks are correctly named!")
-			else forcedUndetectable.add(value)
+		val newForcedUndetectable = mutableSetOf<MSPMaterial>()
+		plugin.config.getStringList("forcedUndetectable").forEach {
+			newForcedUndetectable.add(MSPMaterial(it))
 		}
+		forcedUndetectable = newForcedUndetectable
 
-		configFile["defaultUndetectable"].asArray().forEach {
-			val value = MSPMaterial(it.asString())
-
-			if (value == null) logger.warning("No Material for $value! Make sure all default undetectable blocks are correctly named!")
-			else defaultUndetectable.add(value)
+		val newDefaultUndetectable = mutableSetOf<MSPMaterial>()
+		plugin.config.getStringList("defaultUndetectable").forEach {
+			newDefaultUndetectable.add(MSPMaterial(it))
 		}
-
-		mainConfig = JsonValue.readHjson(File(plugin.dataFolder, "config.hjson").bufferedReader()).asObject()!!
+		defaultUndetectable = newDefaultUndetectable
 	}
 }
--- src/main/kotlin/starships/screens/InterfaceScreen.kt
+++ src/main/kotlin/starships/screens/InterfaceScreen.kt
@@ -1,8 +1,8 @@
 package io.github.petercrawley.minecraftstarshipplugin.starships.screens
 
-import io.github.petercrawley.minecraftstarshipplugin.MinecraftStarshipPlugin.Companion.itemWithName
 import io.github.petercrawley.minecraftstarshipplugin.events.StarshipPilotEvent
 import io.github.petercrawley.minecraftstarshipplugin.starships.Starship
+import io.github.petercrawley.minecraftstarshipplugin.utils.NamedItem
 import io.github.petercrawley.minecraftstarshipplugin.utils.Screen
 import org.bukkit.Bukkit
 import org.bukkit.Material
@@ -13,9 +13,9 @@ class InterfaceScreen(player: Player, private val starship: Starship) : Screen()
 	init {
 		createScreen(player, InventoryType.HOPPER, "Starship Interface")
 
-		screen.setItem(0, itemWithName(Material.MINECART, "Detect Starship", 128, 255, 128, bold = true))
-		screen.setItem(1, itemWithName(Material.COMPASS, "Pilot Starship", 128, 128, 255, bold = true))
-		screen.setItem(4, itemWithName(Material.BEDROCK, "Allow Undetectables", 255, 128, 128, bold = true))
+		screen.setItem(0, NamedItem(Material.MINECART, "Detect Starship",     false, 128, 255, 128, bold = true))
+		screen.setItem(1, NamedItem(Material.COMPASS,  "Pilot Starship",      false, 128, 128, 255, bold = true))
+		screen.setItem(4, NamedItem(Material.BEDROCK,  "Allow Undetectables", false, 255, 128, 128, bold = true))
 	}
 
 	override fun onScreenButtonClicked(slot: Int) {
