92835a490ce2bbeb8a22edb8e211a967b45db8b1
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2021-11-14 11:24:44 -0700

    Clean up projectiles a little

--- src/main/kotlin/inactiveCode/projectiles/ParticleProjectile.kt
+++ src/main/kotlin/inactiveCode/projectiles/ParticleProjectile.kt
@@ -5,10 +5,9 @@ import io.github.petercrawley.minecraftstarshipplugin.MinecraftStarshipPlugin.Co
 import io.github.petercrawley.minecraftstarshipplugin.events.ParticleProjectileHitBlockEvent
 import io.github.petercrawley.minecraftstarshipplugin.events.ParticleProjectileHitEntityEvent
 import io.github.petercrawley.minecraftstarshipplugin.events.ParticleProjectileLaunchEvent
-import org.bukkit.Bukkit
+import org.bukkit.Bukkit.getPluginManager
 import org.bukkit.Color
 import org.bukkit.Location
-import org.bukkit.Material
 import org.bukkit.Particle
 import org.bukkit.entity.LivingEntity
 import org.bukkit.util.Vector
@@ -21,17 +20,18 @@ data class ParticleProjectile (val origin: Location, private val color: Color, p
 	// ParticleBuilder that we can spawn later when we need to
 
 	fun shootBeam() {
-		val event = ParticleProjectileLaunchEvent(this, origin)
-		Bukkit.getPluginManager().callEvent(event)
-
 		// Simple beam mode, do it all on the same server tick
+
+		val event = ParticleProjectileLaunchEvent(this, origin)
+		getPluginManager().callEvent(event)
+
 		val loc = origin.clone() // Don't want to modify the origin, so we clone it
 		val direction: Vector = origin.direction // Main reason this uses Locations (even though they are slow) is for this
 		// Basically represent the amount we move every step
 		direction.multiply(range)
 		direction.normalize()
 
-		for (i in 0..range) { // Go tick at every location in the range, during this server tick
+		for (i in 0..range) { // Go step at every location in the range, during this server tick
 			loc.add(direction)
 			if (!step(loc)) break // if it returned false it hit something
 		}
@@ -39,8 +39,9 @@ data class ParticleProjectile (val origin: Location, private val color: Color, p
 
 	fun shootProjectile() {
 		// Start the Runnable that will handle projectile mode
+
 		val event = ParticleProjectileLaunchEvent(this, origin)
-		Bukkit.getPluginManager().callEvent(event)
+		getPluginManager().callEvent(event)
 
 		ProjectileRunnable(this).runTaskTimer(plugin, 1, 1)
 	}
@@ -51,34 +52,37 @@ data class ParticleProjectile (val origin: Location, private val color: Color, p
 		particle.location(loc).spawn() // actually spawn the particle
 
 		for (e in loc.world.getNearbyEntities(loc, 1.0, 1.0, 1.0)) {
-			if (e is LivingEntity && origin.distance(loc) > minRange) {
-				// minRange exists so the person shooting (if its a gun or something)
-				// or the ship shooting don't blow themselves to bits. Its not a great system,
+			if (e is LivingEntity && origin.distanceSquared(loc) > minRange * minRange) {
+				// minRange exists so the person shooting (if it's a gun or something)
+				// or the ship shooting don't blow themselves to bits. It's not a great system,
 				// but it works. We can't just exclude the player that shot it because we don't know if it *is* a
 				// player who shot it.
+
+				// TODO: Exclude the entire starship/player that shot the projectile, instead of dealing with minRange
+
 				e.damage(damage)
 				if (explosion > 0) loc.world.createExplosion(loc, explosion)
 
 				val event = ParticleProjectileHitEntityEvent(this, loc, e)
-				Bukkit.getPluginManager().callEvent(event)
+				getPluginManager().callEvent(event)
 
 				return false // damage one entity only
 			}
 		}
 
-		if (loc.block.type != Material.AIR) {
-			if (explosion > 0) { // Sure this could be one line but checking distance is costly and we should only do it if we have to
-				if (origin.distance(loc) > minRange) { // distance() might return NaN if the distance is too high...
-					loc.world.createExplosion(loc, explosion) // As far as I can tell this is the main cause of lag when spamming these
-				}
+		if (loc.block.type.isSolid) {
+			if (explosion > 0 && origin.distanceSquared(loc) > minRange * minRange) {
+				loc.world.createExplosion(loc, explosion)
 			}
 
 			// Alert any listeners
 			val event = ParticleProjectileHitBlockEvent(this, loc)
-			Bukkit.getPluginManager().callEvent(event)
+			getPluginManager().callEvent(event)
 
 			return false
 		}
+
+		// If nothing stopped the beam, we're good to continue
 		return true
 	}
 }
