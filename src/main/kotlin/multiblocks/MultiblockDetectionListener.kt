68672ece4a7d2f835a30bf8432811e858e49558f
Peter-Crawley <peterzcrawley@gmail.com>
2021-11-28 14:52:12 +0000

    Listen for multiblock detections

--- src/main/kotlin/multiblocks/MultiblockDetectionListener.kt
+++ src/main/kotlin/multiblocks/MultiblockDetectionListener.kt
@@ -1,6 +1,5 @@
 package io.github.petercrawley.minecraftstarshipplugin.multiblocks
 
-import io.github.petercrawley.minecraftstarshipplugin.customMaterials.MSPMaterial
 import org.bukkit.block.Sign
 import org.bukkit.block.data.type.WallSign
 import org.bukkit.event.Listener
@@ -10,8 +9,17 @@ class MultiblockDetectionListener: Listener {
 	fun onMultiblockDetection(event: PlayerInteractEvent) {
 		if (event.clickedBlock == null) return // Interacted with air, we don't care
 
-		val block = MSPMaterial(event.clickedBlock!!.blockData)
+		val block = event.clickedBlock!!.blockData as Sign
 
-		if (block != MSPMaterial("INTERFACE")) return // Not an interface block
+		if (block.line(0).toString() != "[multiblock]") return // Not a multiblock sign, we don't care
+
+		if (event.clickedBlock !is WallSign) { // Not a wall sign, we don't care
+			event.player.sendMessage("Only wall signs can be multiblock signs")
+			return
+		}
+
+
+
 	}
+
 }
