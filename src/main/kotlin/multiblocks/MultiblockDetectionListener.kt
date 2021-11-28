5ed4081d5f044e60ac5c4ca2b4bee6d7feed9c45
Astralchroma <astralchroma@proton.me>
2021-11-28 17:44:17 +0000

    Use interface not sign

--- src/main/kotlin/multiblocks/MultiblockDetectionListener.kt
+++ src/main/kotlin/multiblocks/MultiblockDetectionListener.kt
@@ -10,17 +10,8 @@ class MultiblockDetectionListener: Listener {
 	fun onMultiblockDetection(event: PlayerInteractEvent) {
 		if (event.clickedBlock == null) return // Interacted with air, we don't care
 
-		val block = event.clickedBlock!!.blockData as Sign
+		val block = MSPMaterial(event.clickedBlock!!.blockData)
 
-		if (block.line(0).toString() != "[multiblock]") return // Not a multiblock sign, we don't care
-
-		if (event.clickedBlock !is WallSign) { // Not a wall sign, we don't care
-			event.player.sendMessage("Only wall signs can be multiblock signs")
-			return
-		}
-
-
-
+		if (block != MSPMaterial("INTERFACE")) return // Not an interface block
 	}
-
 }
