b1db81d0bccb27ed2021ba7611d51c09506bf1b2
Peter-Crawley <peterzcrawley@gmail.com>
2021-11-28 14:52:35 +0000

    Hold multiblock configurations

--- src/main/kotlin/multiblocks/MultiblockConfiguration.kt
+++ src/main/kotlin/multiblocks/MultiblockConfiguration.kt
@@ -2,6 +2,8 @@ package io.github.petercrawley.minecraftstarshipplugin.multiblocks
 
 import io.github.petercrawley.minecraftstarshipplugin.customMaterials.MSPMaterial
 
-data class MultiblockConfiguration(val name: String) {
-	val blocks = mutableMapOf<MultiblockOriginRelativeLocation, MSPMaterial>()
-}
+data class MultiblockConfiguration(
+	val name: String,
+	val blocks: List<List<List<MSPMaterial>>>,
+	val signLocation: MultiblockSignLocation
+)
