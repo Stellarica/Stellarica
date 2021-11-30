50a08c7f4655ccbc8709b139509eeb5be7f04172
Peter-Crawley <peterzcrawley@gmail.com>
2021-11-30 18:50:58 +0000

    Foundation of new MSPMaterial

--- src/main/kotlin/customMaterials/MSPMaterial.kt
+++ src/main/kotlin/customMaterials/MSPMaterial.kt
@@ -1,60 +1,95 @@
 package io.github.petercrawley.minecraftstarshipplugin.customMaterials
 
-import io.github.petercrawley.minecraftstarshipplugin.MinecraftStarshipPlugin.Companion.customBlocks
+import org.bukkit.Bukkit.createBlockData
 import org.bukkit.Material
-import org.bukkit.block.Block
-import org.bukkit.block.BlockFace
 import org.bukkit.block.data.BlockData
-import org.bukkit.block.data.MultipleFacing
+import org.bukkit.inventory.ItemStack
 
-class MSPMaterial(material: Any?) {	
-	var material: Any? = material
-		set(value) {
-			when (value) {
-				is Block, is BlockData -> {
-					val originalMaterial = if (value is Block) value.type else (value as BlockData).material
+enum class MaterialType {
+	CustomBlock,
+	CustomItem,
+	Bukkit
+}
 
-					val blockData = if (value is Block) value.blockData else value
+class MSPMaterial {
+	private var materialType: MaterialType = MaterialType.Bukkit
+	private var material: Any = Material.AIR
 
-					if (originalMaterial == Material.MUSHROOM_STEM) {
-						val block = blockData as MultipleFacing
-						var id = 0
-
-						if (block.hasFace(BlockFace.NORTH)) id += 32
-						if (block.hasFace(BlockFace.EAST)) id += 16
-						if (block.hasFace(BlockFace.SOUTH)) id += 8
-						if (block.hasFace(BlockFace.WEST)) id += 4
-						if (block.hasFace(BlockFace.UP)) id += 2
-						if (block.hasFace(BlockFace.DOWN)) id += 1
-
-						field = customBlocks.getOrDefault(id.toByte(), "MUSHROOM_STEM")
-
-						if (material == "MUSHROOM_STEM") field = originalMaterial
-
-					} else field = originalMaterial
-				}
-				is Material -> field = value
-				is String -> field = Material.getMaterial(value) ?: if (customBlocks.values.contains(value)) value else null
-				else -> field = null
-			}
-		}
-
-	fun bukkit(): Material? {
-		return if (material is Material) material as Material else null
+	constructor(material: Material) {
+		this.materialType = MaterialType.Bukkit
+		this.material = material
 	}
 
-	override fun equals(other: Any?): Boolean {
-		if (this === other) return true
-		if (javaClass != other?.javaClass) return false
-
-		other as MSPMaterial
-
-		if (material != other.material) return false
-
-		return true
+	constructor(material: Byte) {
+		this.materialType = MaterialType.CustomBlock
+		this.material = material
 	}
 
-	override fun hashCode(): Int {
-		return material?.hashCode() ?: 0
+	constructor(material: Int) {
+		this.materialType = MaterialType.CustomItem
+		this.material = material
+	}
+
+	fun getBukkitMaterial(): Material {
+		return Material.AIR
+	}
+
+	fun getBukkitBlockData(): BlockData {
+		return createBlockData(Material.AIR)
+	}
+
+	fun getBukkitItemStack(): ItemStack {
+		return ItemStack(Material.AIR)
 	}
 }
+
+//	var material: Any? = material
+//		set(value) {
+//			when (value) {
+//				is Block, is BlockData -> {
+//					val originalMaterial = if (value is Block) value.type else (value as BlockData).material
+//
+//					val blockData = if (value is Block) value.blockData else value
+//
+//					if (originalMaterial == Material.MUSHROOM_STEM) {
+//						val block = blockData as MultipleFacing
+//						var id = 0
+//
+//						if (block.hasFace(BlockFace.NORTH)) id += 32
+//						if (block.hasFace(BlockFace.EAST)) id += 16
+//						if (block.hasFace(BlockFace.SOUTH)) id += 8
+//						if (block.hasFace(BlockFace.WEST)) id += 4
+//						if (block.hasFace(BlockFace.UP)) id += 2
+//						if (block.hasFace(BlockFace.DOWN)) id += 1
+//
+//						field = customBlocks.getOrDefault(id.toByte(), "MUSHROOM_STEM")
+//
+//						if (material == "MUSHROOM_STEM") field = originalMaterial
+//
+//					} else field = originalMaterial
+//				}
+//				is Material -> field = value
+//				is String -> field = Material.getMaterial(value) ?: if (customBlocks.values.contains(value)) value else null
+//				else -> field = null
+//			}
+//		}
+//
+//	fun bukkit(): Material? {
+//		return if (material is Material) material as Material else null
+//	}
+//
+//	override fun equals(other: Any?): Boolean {
+//		if (this === other) return true
+//		if (javaClass != other?.javaClass) return false
+//
+//		other as MSPMaterial
+//
+//		if (material != other.material) return false
+//
+//		return true
+//	}
+//
+//	override fun hashCode(): Int {
+//		return material?.hashCode() ?: 0
+//	}
+//}
