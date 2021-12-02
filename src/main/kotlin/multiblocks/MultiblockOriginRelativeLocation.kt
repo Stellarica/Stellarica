83f0bacadbf9e256b52962137a937519298b57ec
Astralchroma <astralchroma@proton.me>
2021-12-02 18:05:22 +0000

    toString functions for debugging

--- src/main/kotlin/multiblocks/MultiblockOriginRelativeLocation.kt
+++ src/main/kotlin/multiblocks/MultiblockOriginRelativeLocation.kt
@@ -1,3 +1,7 @@
 package io.github.petercrawley.minecraftstarshipplugin.multiblocks
 
-data class MultiblockOriginRelativeLocation (val x: Int, val y: Int, val z: Int)
+data class MultiblockOriginRelativeLocation (val x: Int, val y: Int, val z: Int) {
+	override fun toString(): String {
+		return "($x, $y, $z)"
+	}
+}
