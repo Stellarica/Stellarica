1a3a3ff5467f217249edeb08b4048369c74945f1
Peter-Crawley <peterzcrawley@gmail.com>
2021-12-02 21:09:04 +0000

    Various bug fixes

--- src/main/kotlin/multiblocks/MultiblockDetectionListener.kt
+++ src/main/kotlin/multiblocks/MultiblockDetectionListener.kt
@@ -1,15 +1,23 @@
 package io.github.petercrawley.minecraftstarshipplugin.multiblocks
 
+import io.github.petercrawley.minecraftstarshipplugin.MinecraftStarshipPlugin.Companion.plugin
 import io.github.petercrawley.minecraftstarshipplugin.customMaterials.MSPMaterial
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
 
 class MultiblockDetectionListener: Listener {
+	@EventHandler
 	fun onMultiblockDetection(event: PlayerInteractEvent) {
-		if (event.clickedBlock == null) return // Interacted with air, we don't care
+		if (event.action != Action.RIGHT_CLICK_BLOCK) return
 
 		val block = MSPMaterial(event.clickedBlock!!.blockData)
 
 		if (block != MSPMaterial("INTERFACE")) return // Not an interface block
+
+		event.isCancelled = true
+
+		plugin.logger.info("Interface Activation")
 	}
 }
