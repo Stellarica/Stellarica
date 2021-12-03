c122916ad882276c3b7bbaaad4d83891e45f03b6
PeterCrawley1 <peterzcrawley@gmail.com>
2021-12-03 11:23:57 +0000

    Switch to using MSPConfigReloadEvent

--- src/main/kotlin/multiblocks/MultiblockDetectionListener.kt
+++ src/main/kotlin/multiblocks/MultiblockDetectionListener.kt
@@ -2,12 +2,18 @@ package io.github.petercrawley.minecraftstarshipplugin.multiblocks
 
 import io.github.petercrawley.minecraftstarshipplugin.MinecraftStarshipPlugin.Companion.plugin
 import io.github.petercrawley.minecraftstarshipplugin.customMaterials.MSPMaterial
+import io.github.petercrawley.minecraftstarshipplugin.events.MSPConfigReloadEvent
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
 
 class MultiblockDetectionListener: Listener {
+	companion object {
+		var multiblocks = setOf<MultiblockConfiguration>()
+			private set
+	}
+
 	@EventHandler
 	fun onMultiblockDetection(event: PlayerInteractEvent) {
 		if (event.action != Action.RIGHT_CLICK_BLOCK) return
@@ -19,5 +25,88 @@ class MultiblockDetectionListener: Listener {
 		event.isCancelled = true
 
 		plugin.logger.info("Interface Activation")
+	}
+
+	@EventHandler
+	fun onMSPConfigReload(event: MSPConfigReloadEvent) {
+		val newMultiblocks = mutableSetOf<MultiblockConfiguration>()
+		plugin.config.getConfigurationSection("multiblocks")?.getKeys(false)?.forEach multiblockLoop@{ multiblock ->
+			// The first thing that needs to be done is we need to get all the keys for the multiblock
+			// This way we know what blocks are in the multiblock
+			val keys = mutableMapOf<String, MSPMaterial>()
+			var interfaceKey: Char? = null
+
+			plugin.config.getConfigurationSection("multiblocks.$multiblock.key")!!.getKeys(false).forEach {
+				val materialString = plugin.config.getString("multiblocks.$multiblock.key.$it")!!
+
+				val material = MSPMaterial(materialString)
+
+				if (keys.containsValue(material)) {
+					plugin.logger.severe("Multiblock $multiblock contains duplicate material $materialString")
+					return@multiblockLoop
+				}
+
+				// TODO: Interface should be determined by a config file.
+				if (materialString == "INTERFACE") interfaceKey = it[0]
+
+				keys[it] = material
+			}
+
+			if (interfaceKey == null) {
+				plugin.logger.severe("Multiblock $multiblock does not have an interface block")
+				return@multiblockLoop
+			}
+
+			// Now we need to find the interface as all blocks in a multtiblock are stored relative to this point.
+			val layers = plugin.config.getConfigurationSection("multiblocks.$multiblock.layers")!!.getKeys(false)
+
+			var interfaceY: Int? = null
+			var interfaceZ: Int? = null
+			var interfaceX: Int? = null
+
+			run layerLoop@ {
+				layers.forEachIndexed { y, yName ->
+					plugin.config.getStringList("multiblocks.$multiblock.layers.$yName").forEachIndexed { z, zString ->
+						zString.forEachIndexed { x, xChar ->
+							if (xChar == interfaceKey) {
+								interfaceY = y
+								interfaceZ = z
+								interfaceX = x
+
+								return@layerLoop
+							}
+						}
+					}
+				}
+			}
+
+			// Create a MultiblockConfiguration
+			val multiblockConfiguration = MultiblockConfiguration(multiblock)
+
+			// Now we need to get all the blocks relative to the origin (interface)
+			layers.forEachIndexed { y, yName ->
+				plugin.config.getStringList("multiblocks.$multiblock.layers.$yName").forEachIndexed { z, zString ->
+					zString.forEachIndexed { x, xChar ->
+						// Find relative position
+						val relativeY = y - interfaceY!!
+						val relativeZ = z - interfaceZ!!
+						val relativeX = x - interfaceX!!
+
+						// Get the material from keys
+						val material = keys[xChar.toString()]
+
+						// Construct a MultiblockOriginRelativeLocation
+						val location = MultiblockOriginRelativeLocation(relativeX, relativeY, relativeZ)
+
+						// Add the block to the multiblock configuration
+						multiblockConfiguration.blocks[location] = material!!
+					}
+				}
+			}
+
+			newMultiblocks.add(multiblockConfiguration)
+		}
+
+		multiblocks = newMultiblocks
 	}
 }
