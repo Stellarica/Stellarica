d8a28617bbe4b6c2afb5983373556ea874058cc7
trainb0y1 <trainb0ydev@gmail.com>
2022-04-02 10:20:41 -0600

    Actually unpilot ship

--- src/main/kotlin/io/github/hydrazinemc/hydrazine/starships/screens/InterfaceScreen.kt
+++ src/main/kotlin/io/github/hydrazinemc/hydrazine/starships/screens/InterfaceScreen.kt
@@ -3,6 +3,7 @@ package io.github.hydrazinemc.hydrazine.starships.screens
 import io.github.hydrazinemc.hydrazine.starships.Starship
 import io.github.hydrazinemc.hydrazine.utils.NamedItem
 import io.github.hydrazinemc.hydrazine.utils.Screen
+import io.github.hydrazinemc.hydrazine.utils.isPilotingShip
 import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryType
@@ -12,7 +13,12 @@ class InterfaceScreen(player: Player, private val starship: Starship) : Screen()
 		createScreen(player, InventoryType.HOPPER, "Starship Interface")
 
 		screen.setItem(0, NamedItem(Material.MINECART, "Detect Starship", false, 128, 255, 128, bold = true))
-		screen.setItem(1, NamedItem(Material.COMPASS, "Pilot Starship", false, 128, 128, 255, bold = true))
+		if (player.isPilotingShip) {
+			screen.setItem(1, NamedItem(Material.COMPASS, "Unpilot Starship", false, 128, 128, 255, bold = true))
+		}
+		else {
+			screen.setItem(1, NamedItem(Material.COMPASS, "Pilot Starship", false, 128, 128, 255, bold = true))
+		}
 		screen.setItem(4, NamedItem(Material.BEDROCK, "Allow Undetectables", false, 255, 128, 128, bold = true))
 	}
 
@@ -20,12 +26,8 @@ class InterfaceScreen(player: Player, private val starship: Starship) : Screen()
 		when (slot) {
 			0 -> starship.detectStarship(player)
 			1 -> {
-				starship.activateStarship(player)
-
-				// Not exactly sure where the best place for this is, as piloting doesn't seem to be quite done, but for now, here should work
-				// val event = StarshipPilotEvent(starship, player)
-				// Bukkit.getPluginManager().callEvent(event)
-
+				if (player.isPilotingShip) starship.deactivateStarship()
+				else starship.activateStarship(player)
 				closeScreen()
 			}
 			4 -> {
