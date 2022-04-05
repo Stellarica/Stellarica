1a42152d6a7490d86fca8bb65f70e0f07b4adc30
trainb0y <trainb0ydev@gmail.com>
2022-04-04 19:12:34 -0600

    Beginnings of a hotbar menu system

--- src/main/kotlin/io/github/hydrazinemc/hydrazine/utils/extensions/playerextensions.kt
+++ src/main/kotlin/io/github/hydrazinemc/hydrazine/utils/extensions/playerextensions.kt
@@ -3,6 +3,7 @@ package io.github.hydrazinemc.hydrazine.utils.extensions
 import io.github.hydrazinemc.hydrazine.Hydrazine.Companion.activeStarships
 import io.github.hydrazinemc.hydrazine.starships.Starship
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 
 /**
  * The starship the player is riding
@@ -26,3 +27,19 @@ val Player.starship: Starship?
  */
 val Player.isPilotingShip: Boolean
 	get() = this.starship?.pilot == this
+
+fun Player.setHotbar(bar: MutableList<ItemStack?>) {
+	for (i in 0..9) {
+		this.inventory.setItem(i, bar[i])
+	}
+}
+
+val Player.hotbar: MutableList<ItemStack?>
+	get() {
+		// this could be code golfed
+		val bar = mutableListOf<ItemStack?>()
+		for (i in 0..9) {
+			bar[i] = this.inventory.getItem(i)
+		}
+		return bar
+	}
