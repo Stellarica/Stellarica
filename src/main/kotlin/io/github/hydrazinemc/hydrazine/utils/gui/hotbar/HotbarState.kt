7938ae9174cff9f6eab53ca20b1a501ad52fcddc
trainb0y1 <trainb0ydev@gmail.com>
2022-04-13 09:04:16 -0600

    Separate HotbarMenu and HotbarState

---
+++ src/main/kotlin/io/github/hydrazinemc/hydrazine/utils/gui/hotbar/HotbarState.kt
@@ -0,0 +1,9 @@
+package io.github.hydrazinemc.hydrazine.utils.gui.hotbar
+
+import org.bukkit.inventory.ItemStack
+
+data class HotbarState(
+	var originalHotbar: MutableList<ItemStack?>,
+	var menuHotbar: MutableList<ItemStack?>,
+	var isMenuOpen: Boolean
+	)
