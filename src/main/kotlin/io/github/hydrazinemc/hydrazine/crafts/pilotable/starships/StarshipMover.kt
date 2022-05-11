3aa0a6b80c4e84a4f04a569271995f5a923cd01c
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2022-05-11 10:23:50 -0600

    Really bad move timing

--- src/main/kotlin/io/github/hydrazinemc/hydrazine/crafts/pilotable/starships/StarshipMover.kt
+++ src/main/kotlin/io/github/hydrazinemc/hydrazine/crafts/pilotable/starships/StarshipMover.kt
@@ -1,6 +1,7 @@
 package io.github.hydrazinemc.hydrazine.crafts.pilotable.starships
 
 import io.github.hydrazinemc.hydrazine.Hydrazine.Companion.pilotedCrafts
+import io.github.hydrazinemc.hydrazine.Hydrazine.Companion.plugin
 import io.github.hydrazinemc.hydrazine.utils.Vector3
 import org.bukkit.scheduler.BukkitRunnable
 
@@ -10,23 +11,31 @@ import org.bukkit.scheduler.BukkitRunnable
  */
 object StarshipMover : BukkitRunnable() {
 
+	var tickCounter = 0
+		private set
+
+	val movesPerSecond: Float
+		get() = 20 / ticksPerMove.toFloat()
+
+	var ticksPerMove = 0
+		private set
 	/**
 	 * Should not be called manually, as this is part of a Bukkit runnable.
-	 *
-	 * Moves all starships with a non-zero velocity
 	 */
 	override fun run() {
-		pilotedCrafts.forEach {ship ->
-			if (ship !is Starship) return@forEach
-			if (ship.velocty == Vector3.zero) return@forEach
-			if (ship.isMoving) return@forEach
-			ship.queueMovement(ship.velocty.asBlockLocation)
-			ship.messagePilot("""
-				
-				<gray>Ship Movement!
-				|Velocity: ${ship.velocty}
-				|Acceleration: ${ship.acceleration}
-			""".trimIndent())
+		tickCounter++
+		if (tickCounter >= ticksPerMove) {
+			pilotedCrafts.forEach {ship ->
+				if (ship !is Starship) return@forEach
+				if (ship.velocty == Vector3.zero) return@forEach
+				if (ship.isMoving) return@forEach
+
+				ship.queueMovement((ship.velocty / movesPerSecond).asBlockLocation)
+				ship.messagePilot("|Velocity: <bold>(${ship.velocty.x}, ${ship.velocty.y}, ${ship.velocty.z})")
+				ship.messagePilot("<gray>$movesPerSecond moves per second")
+			}
+			tickCounter = 0
+			ticksPerMove = plugin.server.averageTickTime.toInt()
 		}
 	}
 }
