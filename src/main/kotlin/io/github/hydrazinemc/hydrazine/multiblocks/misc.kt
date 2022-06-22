4aa408173c541e299eeda250858f000e85223d0b
trainb0y <trainb0ydev@gmail.com>
2022-06-22 09:03:21 -0600

    Break up misc.kt

--- src/main/kotlin/io/github/hydrazinemc/hydrazine/multiblocks/misc.kt
+++ src/main/kotlin/io/github/hydrazinemc/hydrazine/multiblocks/misc.kt
@@ -5,53 +5,6 @@ import org.bukkit.block.Block
 import org.bukkit.block.data.MultipleFacing
 
 /**
- * A layout of blocks that represents a valid multiblock shape
- */
-data class MultiblockLayout(
-	/**
-	 * The name of the layout
-	 */
-	val name: String
-) {
-	/**
-	 * The ids of the origin relative blocks
-	 * @see getId
-	 */
-	val blocks = mutableMapOf<MultiblockOriginRelative, String>()
-}
-
-/**
- * Data for a multiblock instance
- */
-data class Multiblock(
-	/**
-	 * The name of the [MultiblockLayout]
-	 */
-	val name: String,
-	/**
-	 * The x coordinate of the origin
-	 */
-	val x: Int,
-	/**
-	 * The y coordinate of the origin
-	 */
-	val y: Int,
-	/**
-	 * The z coordinate of the origin
-	 */
-	val z: Int,
-	/**
-	 * The amount of rotation
-	 */
-	val r: Byte,
-	/**
-	 * The number of ticks since the multiblock did something?
-	 * Not sure; leftover from MSP
-	 */
-	var t: Int = 0
-)
-
-/**
  * Get the custom block or material id of the block
  */
 fun getId(block: Block): String = (
