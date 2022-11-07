8c8c735a151a5fc03bd08e8a43ab4fce6abebcd4
trainb0y <trainb0ydev@gmail.com>
2022-11-06 21:06:33 -0700

    (barely) functional ship weapons

---
+++ src/main/kotlin/io/github/hydrazinemc/hydrazine/crafts/pilotable/starships/subsystem/weapons/projectiles/Projectile.kt
@@ -0,0 +1,7 @@
+package io.github.hydrazinemc.hydrazine.crafts.pilotable.starships.subsystem.weapons.projectiles;
+
+import org.bukkit.Location;
+
+interface Projectile {
+	fun shoot(origin:Location)
+}
---
+++ src/main/kotlin/io/github/hydrazinemc/hydrazine/crafts/pilotable/starships/subsystem/weapons/projectiles/TestProjectile.kt
@@ -0,0 +1,22 @@
+package io.github.hydrazinemc.hydrazine.crafts.pilotable.starships.subsystem.weapons.projectiles
+
+import org.bukkit.Location
+import org.bukkit.Particle
+
+object TestProjectile: Projectile {
+	override fun shoot(origin: Location) {
+		origin.world.rayTraceBlocks(origin.clone().add(origin.direction), origin.direction, 100.0)?.let {
+			val loc = it.hitPosition.toLocation(origin.world)
+			origin.world.createExplosion(loc, 5.0f)
+			loc.world.spawnParticle(Particle.SOUL_FIRE_FLAME, loc, 1, 0.0, 0.0, 0.0, 0.0, null, true)
+
+			val particleLoc = origin.clone()
+			val path = loc.clone().subtract(origin)
+			val step = path.clone().multiply(1.0 / path.length())
+			for (i in 1..path.length().toInt()) {
+				particleLoc.add(step)
+				particleLoc.world.spawnParticle(Particle.SOUL_FIRE_FLAME, particleLoc, 1, 0.0, 0.0, 0.0, 0.0, null, true)
+			}
+		}
+	}
+}
