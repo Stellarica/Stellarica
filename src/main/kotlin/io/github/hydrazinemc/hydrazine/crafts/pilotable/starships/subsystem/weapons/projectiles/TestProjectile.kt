92f0562545dd6bf5cd4f4ecd867fca7006e13917
trainb0y <trainb0ydev@gmail.com>
2022-11-08 20:00:55 -0700

    beginnings of a better weapon system

--- src/main/kotlin/io/github/hydrazinemc/hydrazine/crafts/pilotable/starships/subsystem/weapons/projectiles/TestProjectile.kt
+++ src/main/kotlin/io/github/hydrazinemc/hydrazine/crafts/pilotable/starships/subsystem/weapons/projectiles/TestProjectile.kt
@@ -1,22 +1,24 @@
 package io.github.hydrazinemc.hydrazine.crafts.pilotable.starships.subsystem.weapons.projectiles
 
 import org.bukkit.Location
+import org.bukkit.Material
 import org.bukkit.Particle
 
-object TestProjectile: Projectile {
+object TestProjectile: Projectile() {
 	override fun shoot(origin: Location) {
-		origin.world.rayTraceBlocks(origin.clone().add(origin.direction), origin.direction, 100.0)?.let {
-			val loc = it.hitPosition.toLocation(origin.world)
-			origin.world.createExplosion(loc, 5.0f)
-			loc.world.spawnParticle(Particle.SOUL_FIRE_FLAME, loc, 1, 0.0, 0.0, 0.0, 0.0, null, true)
-
-			val particleLoc = origin.clone()
-			val path = loc.clone().subtract(origin)
-			val step = path.clone().multiply(1.0 / path.length())
-			for (i in 1..path.length().toInt()) {
-				particleLoc.add(step)
-				particleLoc.world.spawnParticle(Particle.SOUL_FIRE_FLAME, particleLoc, 1, 0.0, 0.0, 0.0, 0.0, null, true)
+		cast(origin.clone(), 100, 40, 5,
+			{
+				it.world.spawnParticle(Particle.FLAME, it, 1, 0.0, 0.0, 0.0, 0.0, null, true)
+				false
+			},
+			{
+				it.world.spawnParticle(Particle.SOUL_FIRE_FLAME, it, 1, 0.0, 0.0, 0.0, 0.0, null, true)
+				false
+			},
+			{
+				it.hitBlock?.type = Material.LAVA
+				true
 			}
-		}
+		)
 	}
 }
