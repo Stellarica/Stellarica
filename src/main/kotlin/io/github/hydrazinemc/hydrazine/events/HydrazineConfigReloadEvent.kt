0882de611ecefc77ac132bd2e2d1437c5ad706af
trainb0y <trainb0ydev@gmail.com>
2022-11-12 10:48:24 -0700

    Fix events (companion objects are not inheritable)

--- src/main/kotlin/io/github/hydrazinemc/hydrazine/events/HydrazineConfigReloadEvent.kt
+++ src/main/kotlin/io/github/hydrazinemc/hydrazine/events/HydrazineConfigReloadEvent.kt
@@ -1,6 +1,21 @@
 package io.github.hydrazinemc.hydrazine.events
 
+import org.bukkit.event.Event
+import org.bukkit.event.HandlerList
+
 /**
  * Called when the configuration file is reloaded
  */
-class HydrazineConfigReloadEvent : HydrazineEvent()
+class HydrazineConfigReloadEvent : Event() {
+	override fun getHandlers(): HandlerList {
+		return handlerList
+	}
+
+	companion object {
+		/**
+		 * The handlers for this event
+		 */
+		@JvmStatic // Apparently this is required for Paper to recognize this
+		val handlerList = HandlerList()
+	}
+}
--- src/main/kotlin/io/github/hydrazinemc/hydrazine/multiblocks/events/MultiblockLoadEvent.kt
+++ src/main/kotlin/io/github/hydrazinemc/hydrazine/multiblocks/events/MultiblockLoadEvent.kt
@@ -1,6 +1,19 @@
 package io.github.hydrazinemc.hydrazine.multiblocks.events
 
-import io.github.hydrazinemc.hydrazine.events.HydrazineEvent
 import io.github.hydrazinemc.hydrazine.multiblocks.MultiblockInstance
+import org.bukkit.event.Event
+import org.bukkit.event.HandlerList
 
-class MultiblockLoadEvent(val multiblock: MultiblockInstance): HydrazineEvent()
+class MultiblockLoadEvent(val multiblock: MultiblockInstance): Event() {
+	override fun getHandlers(): HandlerList {
+		return handlerList
+	}
+
+	companion object {
+		/**
+		 * The handlers for this event
+		 */
+		@JvmStatic // Apparently this is required for Paper to recognize this
+		val handlerList = HandlerList()
+	}
+}
