ac1abba7e36bd65de3eceadc790da35e8f0f611a
trainb0y <trainb0ydev@gmail.com>
2022-11-12 10:48:24 -0700

    Fix events (companion objects are not inheritable)

--- src/main/kotlin/io/github/hydrazinemc/hydrazine/multiblocks/events/MultiblockDetectEvent.kt
+++ src/main/kotlin/io/github/hydrazinemc/hydrazine/multiblocks/events/MultiblockDetectEvent.kt
@@ -1,6 +1,30 @@
 package io.github.hydrazinemc.hydrazine.multiblocks.events
 
-import io.github.hydrazinemc.hydrazine.events.HydrazineCancellableEvent
 import io.github.hydrazinemc.hydrazine.multiblocks.MultiblockInstance
+import org.bukkit.event.Cancellable
+import org.bukkit.event.Event
+import org.bukkit.event.HandlerList
 
-class MultiblockDetectEvent(val multiblock: MultiblockInstance): HydrazineCancellableEvent()
+class MultiblockDetectEvent(val multiblock: MultiblockInstance): Event(), Cancellable {
+
+	private var isCancelled: Boolean = false
+	override fun setCancelled(cancel: Boolean) {
+		isCancelled = cancel
+	}
+
+	override fun isCancelled(): Boolean {
+		return isCancelled
+	}
+
+	override fun getHandlers(): HandlerList {
+		return handlerList
+	}
+
+	companion object {
+		/**
+		 * The handlers for this event
+		 */
+		@JvmStatic // Apparently this is required for Paper to recognize this
+		val handlerList = HandlerList()
+	}
+}
