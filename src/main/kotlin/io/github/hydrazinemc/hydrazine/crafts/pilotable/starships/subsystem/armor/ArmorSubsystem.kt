cfc4b1669715646f4f731bcb99ca93c7646b9817
trainb0y <trainb0ydev@gmail.com>
2022-11-19 15:06:02 -0700

    tiny changes

--- src/main/kotlin/io/github/hydrazinemc/hydrazine/crafts/pilotable/starships/subsystem/armor/ArmorSubsystem.kt
+++ src/main/kotlin/io/github/hydrazinemc/hydrazine/crafts/pilotable/starships/subsystem/armor/ArmorSubsystem.kt
@@ -2,7 +2,14 @@ package io.github.hydrazinemc.hydrazine.crafts.pilotable.starships.subsystem.arm
 
 import io.github.hydrazinemc.hydrazine.crafts.pilotable.starships.Starship
 import io.github.hydrazinemc.hydrazine.crafts.pilotable.starships.subsystem.Subsystem
+import io.github.hydrazinemc.hydrazine.utils.OriginRelative
 
 class ArmorSubsystem(ship: Starship) : Subsystem(ship) {
+	var armor = mutableMapOf<OriginRelative, Float>()
 
+	override fun onShipPiloted() {
+		ship.detectedBlocks.forEach { loc ->
+			armor[OriginRelative(loc)] = ArmorValues[loc.bukkit.type]
+		}
+	}
 }
