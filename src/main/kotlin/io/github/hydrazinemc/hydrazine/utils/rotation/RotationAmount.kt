535d1d89b60af9e885a86eb82cebc7485b4c0d98
NovaForeverSL <76930542+NovaForeverSL@users.noreply.github.com>
2022-11-20 19:28:27 -0600

    Update RotationAmount.kt

--- src/main/kotlin/io/github/hydrazinemc/hydrazine/utils/rotation/RotationAmount.kt
+++ src/main/kotlin/io/github/hydrazinemc/hydrazine/utils/rotation/RotationAmount.kt
@@ -6,8 +6,8 @@ package io.github.hydrazinemc.hydrazine.utils.rotation
  * @param asDegrees this in degrees
  */
 enum class RotationAmount(val asRadians: Double = 0.0, val asDegrees: Float = 0f) {
-	CLOCKWISE(-Math.PI / 2, -90f),
-	COUNTERCLOCKWISE(Math.PI / 2, 90f),
+	CLOCKWISE(Math.PI / 2, 90f),
+	COUNTERCLOCKWISE(-Math.PI / 2, -90f),
 	REVERSE(Math.PI, 180f),
 	NONE,
 }
