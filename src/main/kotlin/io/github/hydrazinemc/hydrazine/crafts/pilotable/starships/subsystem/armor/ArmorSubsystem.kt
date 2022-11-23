a2ec57c60ab3c83436e0390f51b1d0f2dfb7b90a
trainb0y <66213737+trainb0y@users.noreply.github.com>
2022-11-23 12:54:11 -0700

    Hopefully fix compile
    Indentation is bad because I’m on my phone.

--- src/main/kotlin/io/github/hydrazinemc/hydrazine/crafts/pilotable/starships/subsystem/armor/ArmorSubsystem.kt
+++ src/main/kotlin/io/github/hydrazinemc/hydrazine/crafts/pilotable/starships/subsystem/armor/ArmorSubsystem.kt
@@ -6,10 +6,11 @@ import io.github.hydrazinemc.hydrazine.utils.OriginRelative
 
 class ArmorSubsystem(ship: Starship) : Subsystem(ship) {
 	var armor = mutableMapOf<OriginRelative, Float>()
-
+        /*
 	override fun onShipPiloted() {
 		ship.detectedBlocks.forEach { loc ->
 			armor[OriginRelative(loc)] = ArmorValues[loc.bukkit.type]
 		}
 	}
+        */
 }
