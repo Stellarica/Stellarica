0c31ad1dbb57a938984f08fa0fda1070a2a38f24
trainb0y <trainb0ydev@gmail.com>
2022-12-13 21:42:32 -0700

    lazy possible fix for stuff

--- src/main/kotlin/io/github/hydrazinemc/hydrazine/crafts/pilotable/starships/subsystem/weapons/WeaponSubsystem.kt
+++ src/main/kotlin/io/github/hydrazinemc/hydrazine/crafts/pilotable/starships/subsystem/weapons/WeaponSubsystem.kt
@@ -3,28 +3,25 @@ package io.github.hydrazinemc.hydrazine.crafts.pilotable.starships.subsystem.wea
 import io.github.hydrazinemc.hydrazine.crafts.pilotable.starships.Starship
 import io.github.hydrazinemc.hydrazine.crafts.pilotable.starships.subsystem.Subsystem
 import io.github.hydrazinemc.hydrazine.multiblocks.MultiblockInstance
+import java.lang.ref.WeakReference
 
 class WeaponSubsystem(ship: Starship) : Subsystem(ship) {
 
-	val multiblocks = mutableSetOf<MultiblockInstance>()
+	val multiblocks = mutableSetOf<WeakReference<MultiblockInstance>>()
 
 	override fun onShipPiloted() {
 		ship.multiblocks.forEach { multiblock ->
 			if (multiblock.type in WeaponType.values().map { it.multiblockType }) {
-				multiblocks.add(multiblock)
+				multiblocks.add(WeakReference(multiblock))
 			}
 		}
 	}
 
-	override fun onMultiblockUndetected(multiblock: MultiblockInstance) {
-		multiblocks.remove(multiblock)
-	}
-
 	fun fire() {
-		multiblocks.filter { it.type == WeaponType.TEST_WEAPON.multiblockType }.forEach {
+		multiblocks.filter { it.get()?.type == WeaponType.TEST_WEAPON.multiblockType }.forEach { ref ->
 			WeaponType.TEST_WEAPON.projectile.shoot(
 				ship,
-				it.origin.clone().add(0.5, 0.5, 0.5).add(ship.pilot!!.eyeLocation.direction)
+				ref.get()!!.origin.clone().add(0.5, 0.5, 0.5).add(ship.pilot!!.eyeLocation.direction)
 					.also { it.direction = ship.pilot!!.eyeLocation.direction })
 		}
 	}
