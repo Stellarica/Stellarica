8cb44bc708571007de0e4dbd68386db4e687d288
trainb0y <trainb0ydev@gmail.com>
2022-12-16 10:01:46 -0700

    almost there

--- server/src/main/kotlin/io/github/hydrazinemc/hydrazine/server/crafts/pilotables/starships/subsystems/weapons/WeaponSubsystem.kt
+++ server/src/main/kotlin/io/github/hydrazinemc/hydrazine/server/crafts/pilotables/starships/subsystems/weapons/WeaponSubsystem.kt
@@ -1,10 +1,19 @@
 package io.github.hydrazinemc.hydrazine.server.crafts.pilotables.starships.subsystems.weapons
 
+import DebugProjectile
 import io.github.hydrazinemc.hydrazine.server.crafts.pilotables.starships.Starship
 import io.github.hydrazinemc.hydrazine.server.crafts.pilotables.starships.subsystems.Subsystem
 import io.github.hydrazinemc.hydrazine.server.multiblocks.MultiblockInstance
+import io.github.hydrazinemc.hydrazine.server.utils.extensions.abs
 import io.github.hydrazinemc.hydrazine.server.utils.extensions.sendRichMessage
+import org.bukkit.util.Vector
 import java.lang.ref.WeakReference
+import kotlin.math.PI
+import kotlin.math.abs
+import kotlin.math.atan2
+import kotlin.math.asin
+import kotlin.math.cos
+import kotlin.math.sin
 
 class WeaponSubsystem(ship: Starship) : Subsystem(ship) {
 
@@ -19,16 +28,47 @@ class WeaponSubsystem(ship: Starship) : Subsystem(ship) {
 	}
 
 	fun fire() {
-		val eye = ship.pilot!!.eyeLocation.direction.clone()
+		val eye = ship.pilot!!.eyeLocation.direction.normalize()
 
-		WeaponType.values().sortedBy{it.priority}.forEach {type ->
+		WeaponType.values().sortedBy{it.priority}.forEach { type ->
 			multiblocks.mapNotNull{it.get()}.filter{ it.type == type.multiblockType }.forEach { multiblock ->
-				ship.sendRichMessage(eye.angle(multiblock.getLocation(type.direction).clone().subtract(multiblock.getLocation(type.mount)).toVector()).toString())
+
+
+				val direction = multiblock.getLocation(type.direction).clone().subtract(multiblock.getLocation(type.mount)).toVector().normalize()
+				val angleBetween = abs(eye.angle(direction))
+
+				// if the angleBetween (in radians) is less than the type's cone, fire
+				val dir = if (angleBetween < type.cone + (PI / 4)) {
+					// this whole thing could definitely be improved
+					val relative = eye.clone().subtract(direction).normalize()
+
+					val pitch = asin(-relative.y).coerceIn(-type.cone, type.cone)
+					val yaw = atan2(relative.x, relative.z).coerceIn(-type.cone, type.cone)
+
+					Vector(
+						sin(yaw) * cos(pitch),
+						-sin(pitch),
+						cos(yaw) * cos(pitch),
+					).rotateAroundY(PI - eye.clone().also { it.y = 0.0 }.angle(direction).toDouble())
+				}
+				else return@forEach
+
+				ship.sendRichMessage("Before: $eye, After: $dir")
+
+				DebugProjectile.shoot(ship,
+					multiblock.getLocation(type.mount).clone().add(0.5, 0.5, 0.5).add(direction)
+						.also { it.direction = direction }
+					)
+
+				DebugProjectile.shoot(ship,
+					multiblock.getLocation(type.mount).clone().add(0.5, 0.5, 0.5).add(eye)
+						.also { it.direction = eye }
+				)
 
 				type.projectile.shoot(
 					ship,
-					multiblock.getLocation(type.mount).clone().add(0.5, 0.5, 0.5).add(eye)
-						.also { it.direction = ship.pilot!!.eyeLocation.direction }
+					multiblock.getLocation(type.mount).clone().add(0.5, 0.5, 0.5).add(dir)
+						.also { it.direction = dir }
 				)
 			}
 		}
