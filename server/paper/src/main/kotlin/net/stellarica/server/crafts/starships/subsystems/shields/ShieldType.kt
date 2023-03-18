8887f1ab92fb0dc6e50c27a8dbb8a6a9e7d09b97
trainb0y <trainb0ydev@gmail.com>
2023-03-18 10:54:30 -0600

    Nerf shields again

--- server/paper/src/main/kotlin/net/stellarica/server/crafts/starships/subsystems/shields/ShieldType.kt
+++ server/paper/src/main/kotlin/net/stellarica/server/crafts/starships/subsystems/shields/ShieldType.kt
@@ -4,5 +4,5 @@ import net.stellarica.server.multiblocks.MultiblockType
 import net.stellarica.server.multiblocks.Multiblocks
 
 enum class ShieldType(val maxHealth: Int, val multiblock: MultiblockType) {
-	TEST_SHIELD(400, Multiblocks.TEST_SHIELD);
+	TEST_SHIELD(100, Multiblocks.TEST_SHIELD);
 }
