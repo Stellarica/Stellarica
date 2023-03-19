0c5ce7649b094b4cf29587cb00e3ac9f9ff5692f
trainb0y <trainb0ydev@gmail.com>
2023-03-19 14:54:02 -0600

    Improve formatting of /multiblock types

--- server/paper/src/main/kotlin/net/stellarica/server/multiblocks/MultiblockCommands.kt
+++ server/paper/src/main/kotlin/net/stellarica/server/multiblocks/MultiblockCommands.kt
@@ -2,9 +2,14 @@ package net.stellarica.server.multiblocks
 
 import co.aikar.commands.BaseCommand
 import co.aikar.commands.annotation.CommandAlias
+import co.aikar.commands.annotation.CommandCompletion
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Description
 import co.aikar.commands.annotation.Subcommand
+import net.stellarica.server.StellaricaServer.Companion.identifier
+import net.stellarica.server.multiblocks.matching.BlockTagMatcher
+import net.stellarica.server.multiblocks.matching.MultiBlockMatcher
+import net.stellarica.server.multiblocks.matching.SingleBlockMatcher
 import org.bukkit.command.CommandSender
 
 @Suppress("Unused")
@@ -14,13 +19,32 @@ class MultiblockCommands : BaseCommand() {
 	@Subcommand("types")
 	@Description("Dump multiblock types")
 	@CommandPermission("stellarica.multiblocks.debug")
-	fun onDumpTypes(sender: CommandSender) {
-		Multiblocks.all().forEach { type ->
-			sender.sendRichMessage("")
-			sender.sendRichMessage(type.id.toString())
-			sender.sendRichMessage(type.blocks.entries.joinToString { "${it.key}: ${it.value}\n" })
+	@CommandCompletion("@multiblocks")
+	fun onDumpType(sender: CommandSender, id: String) {
+		val type = Multiblocks.byId(identifier(id)) ?: run {
+			sender.sendRichMessage("<red>No multiblock with the id '$id' found.")
+			return
 		}
+		sender.sendRichMessage("<bold><gold>Multiblock:</bold> ${type.id}")
+
+		// Do you have string formatting nightmares?
+		// If you didn't, now you will
+		// :help_me:
+		sender.sendRichMessage(type.blocks.entries.joinToString("\n") {
+			"""
+			<dark_gray>${"(<gray>${it.key.x}<dark_gray>,<gray> ${it.key.y}<dark_gray>,<gray> ${it.key.z}<dark_gray>)".padEnd(61)} <gray><bold>></bold> <green><bold>${
+				when (it.value) {
+					is MultiBlockMatcher -> "MULTI  </bold><gray>>\n       - " + (it.value as MultiBlockMatcher).types.joinToString("\n       - ") { "<gold>${it.getId().toString().split(":").let { "<gray>${it.first()}:<gold>${it[1]}"}}<gray>," }
+
+					is SingleBlockMatcher -> "SINGLE </bold><gray>> <gold>${(it.value as SingleBlockMatcher).block.getId().toString().split(":").let { "<gray>${it.first()}:<gold>${it[1]}"}}"
+					is BlockTagMatcher -> "TAG    </bold><gray>> <aqua>" + (it.value as BlockTagMatcher).tag.location
+					else -> "<red>Unknown Matcher"
+				}
+			}<reset>
+			""".trimIndent()
+		})
 	}
+
 
 	@Subcommand("loaded")
 	@Description("Dump loaded multiblocks")
--- server/paper/src/main/kotlin/net/stellarica/server/multiblocks/matching/BlockTagMatcher.kt
+++ server/paper/src/main/kotlin/net/stellarica/server/multiblocks/matching/BlockTagMatcher.kt
@@ -5,7 +5,7 @@ import net.minecraft.world.level.block.Block
 import net.stellarica.server.material.type.block.BlockType
 
 @JvmInline
-value class BlockTagMatcher(private val tag: TagKey<Block>): BlockMatcher {
+value class BlockTagMatcher(val tag: TagKey<Block>): BlockMatcher {
 	override fun matches(block: BlockType): Boolean {
 		return block.getVanillaBlockState().tags.anyMatch { it == tag }
 	}
