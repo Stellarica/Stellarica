b9b2393ceec106bd3c4937475f30dea1045f0944
trainb0y <trainb0ydev@gmail.com>
2023-03-18 18:41:08 -0600

    Block matchers

---
+++ server/paper/src/main/kotlin/net/stellarica/server/multiblocks/matching/MultiBlockMatcher.kt
@@ -0,0 +1,8 @@
+package net.stellarica.server.multiblocks.matching
+
+import net.stellarica.server.material.type.block.BlockType
+
+@JvmInline
+value class MultiBlockMatcher(private val types: Set<BlockType>): BlockMatcher {
+	override fun matches(block: BlockType) = block in types
+}
