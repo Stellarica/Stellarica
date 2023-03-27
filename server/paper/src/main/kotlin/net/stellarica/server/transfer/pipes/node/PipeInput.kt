3f6004a003d5a6a6e2682cf2d2864e79bf1968c0
trainb0y <trainb0ydev@gmail.com>
2023-03-27 10:10:16 -0600

    Delete everything and start over (again)

--- server/paper/src/main/kotlin/net/stellarica/server/transfer/pipes/node/PipeInput.kt
+++
@@ -1,8 +0,0 @@
-package net.stellarica.server.transfer.pipes.node
-
-import net.stellarica.common.utils.OriginRelative
-import net.stellarica.server.transfer.Node
-import net.stellarica.server.transfer.pipes.Fuel
-
-class PipeInput(pos: OriginRelative) : PipeNode(pos) {
-}
--- server/paper/src/main/kotlin/net/stellarica/server/transfer/pipes/node/PipeOutput.kt
+++
@@ -1,9 +0,0 @@
-package net.stellarica.server.transfer.pipes.node
-
-import net.stellarica.common.utils.OriginRelative
-import net.stellarica.server.transfer.Node
-import net.stellarica.server.transfer.pipes.Fuel
-
-class PipeOutput(pos: OriginRelative) : PipeNode(pos) {
-
-}
