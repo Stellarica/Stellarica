cd5e3cc26e0993c0a849b3df99c1433175c8f136
trainb0y <trainb0ydev@gmail.com>
2023-04-10 15:54:48 -0600

    No more mixins (won't compile)

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,21 +1,53 @@
 plugins {
-	id("io.papermc.paperweight.userdev") apply false
 	java
+	id("io.papermc.paperweight.userdev")
+	id("com.github.johnrengelman.shadow")
+	kotlin("plugin.serialization")
 }
 
-subprojects {
-	apply(plugin = "io.papermc.paperweight.userdev")
+repositories {
+	maven("https://repo.mineinabyss.com/releases") // ProtocolBurrito
+	maven("https://repo.aikar.co/content/groups/aikar/") // acf-paper
+	maven("https://repo.dmulloy2.net/repository/public/") // ProtocolLib
+	mavenCentral()
+}
 
-	java {
-		toolchain {
-			languageVersion.set(JavaLanguageVersion.of("17"))
-		}
+
+dependencies {
+	implementation(project(":common", "namedElements"))
+
+	paperDevBundle("${property("minecraft_version")}-R0.1-SNAPSHOT")
+	compileOnly("io.papermc.paper:paper-api:${property("minecraft_version")}-R0.1-SNAPSHOT")
+
+	implementation("io.github.microutils:kotlin-logging-jvm:${property("kt_log_version")}")
+
+	implementation("co.aikar:acf-paper:${property("acf_version")}")
+
+	implementation("com.mineinabyss:protocolburrito:${property("protocolburrito_version")}") // Designed to be installed separately but uh.. :cringe:
+	compileOnly("com.comphenix.protocol:ProtocolLib:${property("protocollib_version")}")
+
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${property("kotlin_coroutines_version")}")
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${property("kx_ser_version")}")
+	implementation("org.jetbrains.kotlin:kotlin-reflect:${property("kotlin_version")}")
+}
+
+val version = property("mod_version")!!
+val mc = property("minecraft_version")!!.toString()
+
+tasks {
+	build {
+		dependsOn(reobfJar)
 	}
 
-	tasks {
-		compileKotlin {
-			kotlinOptions.javaParameters = true
-			kotlinOptions.jvmTarget = "17"
+	shadowJar {
+		relocate("co.aikar.commands", "io.github.stellaricamc.stellarica.libraries.co.aikar.commands")
+		relocate("co.aikar.locales", "io.github.stellaricamc.stellarica.libraries.co.aikar.locales")
+	}
+
+	processResources {
+		inputs.property("version", version)
+		filesMatching("plugin.yml") {
+			expand(mutableMapOf("version" to version))
 		}
 	}
 }
