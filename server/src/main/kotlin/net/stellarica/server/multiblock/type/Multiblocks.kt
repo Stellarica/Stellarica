dc2f1065148de673c53a095723a63d4a34747457
trainb0y <trainb0ydev@gmail.com>
2023-04-17 10:16:37 -0600

    Replace multiblock reflection with slightly more cursed reflection

--- server/src/main/kotlin/net/stellarica/server/multiblock/type/Multiblocks.kt
+++ server/src/main/kotlin/net/stellarica/server/multiblock/type/Multiblocks.kt
@@ -2,11 +2,15 @@ package net.stellarica.server.multiblock.type
 
 import net.minecraft.resources.ResourceLocation
 import net.stellarica.server.multiblock.MultiblockType
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.javaGetter
 
 object Multiblocks {
 	val all = MultiblockDef::class.sealedSubclasses.map { def ->
-		def.java.declaredFields.mapNotNull { field ->
-			field.get(def.objectInstance) as? MultiblockType
+		def.memberProperties.mapNotNull { prop ->
+			// cursed, cursed, cursed! there are better reflection-y ways to do this
+			// but this works:tm: for now
+			prop.javaGetter!!.invoke(def.objectInstance!!) as? MultiblockType
 		}
 	}.flatten()
 
