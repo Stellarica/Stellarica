6bb6b4d25f944c84f0efdd1a1c4480e7eb5fb0c3
trainb0y <trainb0ydev@gmail.com>
2023-04-20 12:15:56 -0600

    Sain and puffer (more thruster stuff)

--- server/src/main/kotlin/net/stellarica/server/craft/starship/subsystem/thruster/ThrusterSubsystem.kt
+++ server/src/main/kotlin/net/stellarica/server/craft/starship/subsystem/thruster/ThrusterSubsystem.kt
@@ -1,15 +1,16 @@
 package net.stellarica.server.craft.starship.subsystem.thruster;
 
-import net.minecraft.core.Direction
-import net.minecraft.core.Vec3i
+import net.minecraft.world.phys.Vec3
 import net.stellarica.common.util.OriginRelative
+import net.stellarica.common.util.toVec3
 import net.stellarica.server.craft.starship.Starship
 import net.stellarica.server.craft.starship.subsystem.Subsystem
 import net.stellarica.server.multiblock.data.ThrusterMultiblockData
 import net.stellarica.server.util.extension.div
 import net.stellarica.server.util.extension.plus
 import net.stellarica.server.util.extension.times
-import kotlin.math.roundToInt
+import net.stellarica.server.util.extension.toVector
+import kotlin.math.abs
 
 class ThrusterSubsystem(ship: Starship): Subsystem(ship) {
 	val thrusters = mutableSetOf<OriginRelative>()
@@ -22,11 +23,15 @@ class ThrusterSubsystem(ship: Starship): Subsystem(ship) {
 		}
 	}
 
-	fun stepThrusterWarmup(targetDir: Vec3i) {
+	fun stepThrusterWarmup(targetDir: Vec3) {
 		for (thruster in thrusters.mapNotNull { ship.getMultiblock(it) }) {
 			val type = ThrusterType.values().first { it.multiblock == thruster.type}
 			val data = (thruster.data as ThrusterMultiblockData)
-			if (TODO()) {
+
+			val dir = thruster.direction.normal.toVec3().toVector()
+			val angleBetween = targetDir.toVector().angle(dir)
+
+			if (abs(angleBetween) <= Math.PI / 4) {
 				// warming up
 				data.warmupPercentage += type.warmupSpeed
 			}
@@ -37,14 +42,36 @@ class ThrusterSubsystem(ship: Starship): Subsystem(ship) {
 		}
 	}
 
-	fun calculateTotalThrust(): Vec3i {
-		var thrust = Vec3i.ZERO
+	private fun calculateTotalThrust(): Vec3 {
+		var thrust = Vec3.ZERO
 		for (thruster in thrusters.mapNotNull {ship.getMultiblock(it)}) {
 			val type = ThrusterType.values().first { it.multiblock == thruster.type}
 			val data = (thruster.data as ThrusterMultiblockData)
-			thrust += thruster.direction.normal * (type.maxThrust * data.warmupPercentage)
+			thrust += thruster.direction.normal.toVec3() * (type.maxThrust * data.warmupPercentage).toDouble()
 		}
-		return thrust / ship.mass.roundToInt()
+		return thrust
+	}
+
+	fun calculateActualThrust(dir: Vec3): Vec3 {
+		// though it would be nice to be able to just use the result of calculateTotalThrust as the actual thrust,
+		// depending on how a ship is constructed it might not be able to travel at certain angles
+		// to "fix" that, if the actual thrust is at least somewhat close to the target direction, it will
+		// give thrust in the target direction, with a small penalty to thrust amount.
+		val originalThrust = calculateTotalThrust()
+
+		// painful type conversions aaaaaa
+		val angleBetween = abs(originalThrust.toVector().angle(dir.toVector()))
+
+		if (angleBetween < Math.PI / 8.0) {
+			// no penalty
+			return dir.normalize().scale(originalThrust.length())
+		} else if(angleBetween < Math.PI / 6.0) {
+			// half thrust
+			return dir.normalize().scale(originalThrust.length() / 1.5)
+		}
+
+		// pointing rather far away from the actual thrust, but the actual thrust still applies
+		return originalThrust
 	}
 }
 
--- server/src/main/kotlin/net/stellarica/server/util/extension/vec.kt
+++ server/src/main/kotlin/net/stellarica/server/util/extension/vec.kt
@@ -1,8 +1,8 @@
 package net.stellarica.server.util.extension
 
-import net.minecraft.core.Vec3i
+import net.minecraft.world.phys.Vec3
 
-operator fun Vec3i.plus(other: Vec3i) = this.offset(other)
-operator fun Vec3i.minus(other: Vec3i) = this.subtract(other)
-operator fun Vec3i.times(other: Int) = this.multiply(other)
-operator fun Vec3i.div(other: Int) = this.multiply(1 / other)
+operator fun Vec3.plus(other: Vec3) = this.add(other)
+operator fun Vec3.minus(other: Vec3) = this.subtract(other)
+operator fun Vec3.times(other: Double) = this.scale(other)
+operator fun Vec3.div(other: Double) = this.scale(1 / other)
