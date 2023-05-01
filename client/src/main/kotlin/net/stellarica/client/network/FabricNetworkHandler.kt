88262990c6fc0c1b930252bf8b299396a97450f6
trainb0y <trainb0ydev@gmail.com>
2023-05-01 08:56:34 -0600

    docs because yes

--- client/src/main/kotlin/net/stellarica/client/network/FabricNetworkHandler.kt
+++ client/src/main/kotlin/net/stellarica/client/network/FabricNetworkHandler.kt
@@ -1,24 +1,20 @@
 package net.stellarica.client.network
 
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs
 import net.stellarica.common.networking.Channel
 
 class FabricNetworkHandler {
 	private val listeners = mutableMapOf<ClientboundPacketListener, Long>()
-
-	fun sendPacket(channel: Channel, content: ByteArray) {
-		ClientPlayNetworking.send(channel.fabric, PacketByteBufs.create().also { it.writeByteArray(content) })
+	init {
+		for (channel in Channel.values()) {
+			ClientPlayNetworking.registerGlobalReceiver(channel.fabric) { _, _, buf, _ ->
+				onPacketRecv(channel, buf.accessByteBufWithCorrectSize())
+			}
+		}
 	}
-
-	fun register(listener: ClientboundPacketListener) {
-		listeners[listener] = System.currentTimeMillis()
-	}
-
-	fun unregister(listener: ClientboundPacketListener) {
-		listeners.remove(listener)
-	}
-
 	private fun onPacketRecv(channel: Channel, message: ByteArray) {
 		val current = System.currentTimeMillis()
 		listeners.keys.removeIf { it.timeout != null && listeners[it]!! + it.timeout <= current }
@@ -31,12 +27,25 @@ class FabricNetworkHandler {
 		}
 	}
 
+	/** Send [content] on [channel] to the server */
+	fun sendPacket(channel: Channel, content: ByteArray) {
+		ClientPlayNetworking.send(channel.fabric, PacketByteBufs.create().also { it.writeByteArray(content) })
+	}
 
-	init {
-		for (channel in Channel.values()) {
-			ClientPlayNetworking.registerGlobalReceiver(channel.fabric) { _, _, buf, _ ->
-				onPacketRecv(channel, buf.accessByteBufWithCorrectSize())
-			}
-		}
+	/** Send [obj] (an object serializable with kotlinx.serialization) on [channel] to the server */
+	inline fun <reified T : Any> sendSerializableObject(channel: Channel, obj: T) {
+		sendPacket(channel, Json.encodeToString(obj).toByteArray())
+	}
+
+	/**
+	 * Register [listener]
+	 * If the listener has a timeout, it will expire that many milliseconds after this is called
+	 */
+	fun register(listener: ClientboundPacketListener) {
+		listeners[listener] = System.currentTimeMillis()
+	}
+
+	fun unregister(listener: ClientboundPacketListener) {
+		listeners.remove(listener)
 	}
 }
--- server/src/main/kotlin/net/stellarica/server/networking/BukkitNetworkHandler.kt
+++ server/src/main/kotlin/net/stellarica/server/networking/BukkitNetworkHandler.kt
@@ -9,14 +9,14 @@ import org.bukkit.entity.Player
 import org.bukkit.plugin.messaging.PluginMessageListener
 
 class BukkitNetworkHandler : PluginMessageListener {
+
+	private val listeners = mutableMapOf<ServerboundPacketListener, Long>()
 	init {
 		for (channel in Channel.values()) {
 			plugin.server.messenger.registerIncomingPluginChannel(plugin, channel.bukkit, this)
 			plugin.server.messenger.registerOutgoingPluginChannel(plugin, channel.bukkit)
 		}
 	}
-
-	private val listeners = mutableMapOf<ServerboundPacketListener, Long>()
 
 	override fun onPluginMessageReceived(channelString: String, player: Player, message: ByteArray?) {
 		message ?: return
@@ -38,20 +38,32 @@ class BukkitNetworkHandler : PluginMessageListener {
 		}
 	}
 
+	/** Broadcast [content] on [channel] to all connected modded players */
 	fun broadcastPacket(channel: Channel, packet: ByteArray) {
 		for (player in plugin.moddedPlayers) {
 			sendPacket(channel, player, packet)
 		}
 	}
 
+	/** Broadcast [obj] (an object serializable with kotlinx.serialization) on [channel] to all connected modded players */
+	inline fun <reified T : Any> broadcastSerializableObject(channel: Channel, obj: T) {
+		broadcastPacket(channel, Json.encodeToString(obj).toByteArray())
+	}
+
+	/** Send [content] on [channel] to [player] */
 	fun sendPacket(channel: Channel, player: Player, packet: ByteArray) {
 		player.sendPluginMessage(plugin, channel.bukkit, packet)
 	}
 
+	/** Send [obj] (an object serializable with kotlinx.serialization) on [channel] to [player] */
 	inline fun <reified T : Any> sendSerializableObject(channel: Channel, player: Player, obj: T) {
 		sendPacket(channel, player, Json.encodeToString(obj).toByteArray())
 	}
 
+	/**
+	 * Register [listener]
+	 * If the listener has a timeout, it will expire that many milliseconds after this is called
+	 */
 	fun register(listener: ServerboundPacketListener) {
 		listeners[listener] = System.currentTimeMillis()
 	}
