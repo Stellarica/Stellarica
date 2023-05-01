7668956e449a145e59e9b55fd900a164331cc08d
trainb0y <trainb0ydev@gmail.com>
2023-04-30 18:15:48 -0600

    Login toasts because yes

--- client/src/main/kotlin/net/stellarica/client/StellaricaClient.kt
+++ client/src/main/kotlin/net/stellarica/client/StellaricaClient.kt
@@ -1,11 +1,15 @@
 package net.stellarica.client
 
 import kotlinx.serialization.serializer
+import mu.KotlinLogging
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents.ModifyEntries
+import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.components.toasts.SystemToast
+import net.minecraft.network.chat.Component
 import net.minecraft.resources.ResourceLocation
 import net.stellarica.client.network.ClientboundObjectListener
 import net.stellarica.client.network.ClientboundPacketListener
@@ -19,6 +23,8 @@ import net.stellarica.common.networking.networkVersion
 object StellaricaClient : ClientModInitializer {
 
 	lateinit var networkHandler: FabricNetworkHandler
+
+	val klogger = KotlinLogging.logger("Stellarica")
 
 	val itemGroup = FabricItemGroup.builder(identifier("item_group"))
 			//.icon()
@@ -36,8 +42,33 @@ object StellaricaClient : ClientModInitializer {
 		})
 
 		ClientboundPacketListener(channel = Channel.LOGIN) {
-			println("Received $it from server!")
 			networkHandler.sendPacket(Channel.LOGIN, byteArrayOf(networkVersion))
+
+			val serverVer = it.first()
+			if (networkVersion == serverVer) {
+				// success
+				Minecraft.getInstance().toasts.addToast(SystemToast(
+					SystemToast.SystemToastIds.PERIODIC_NOTIFICATION,
+					Component.literal("Stellarica"),
+					Component.literal("Connected to server!")
+				))
+			}
+			else if (networkVersion < serverVer) {
+				// too old, upgrade
+				Minecraft.getInstance().toasts.addToast(SystemToast(
+					SystemToast.SystemToastIds.PERIODIC_NOTIFICATION,
+					Component.literal("Stellarica"),
+					Component.literal("Outdated client mod version! Some features may not work as intended!")
+				))
+			} else {
+				// too new? downgrade??
+				Minecraft.getInstance().toasts.addToast(SystemToast(
+					SystemToast.SystemToastIds.PERIODIC_NOTIFICATION,
+					Component.literal("Stellarica"),
+					Component.literal("Outdated server plugin version! Some features may not work as intended!")
+				))
+			}
+			klogger.info { "Connected to Stellarica Server! Server Version: $serverVer, Client Version: $networkVersion" }
 			false
 		}.register()
 
