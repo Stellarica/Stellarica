a00523083d6bba3c4061d6327a859e02396fc0d9
trainb0y <trainb0ydev@gmail.com>
2023-05-01 18:25:53 -0600

    New batch of custom items and blocks

--- server/src/main/kotlin/net/stellarica/server/material/custom/block/CustomBlocks.kt
+++ server/src/main/kotlin/net/stellarica/server/material/custom/block/CustomBlocks.kt
@@ -1,16 +1,44 @@
 package net.stellarica.server.material.custom.block
 
 import net.minecraft.resources.ResourceLocation
-import net.stellarica.server.StellaricaServer
+import net.stellarica.server.StellaricaServer.Companion.identifier
 import net.stellarica.server.material.custom.item.CustomItems
 import org.bukkit.Note
 
 @Suppress("unused")
 object CustomBlocks {
 	val ADAMANTITE_BLOCK: CustomBlock = CustomBlock(
-		StellaricaServer.identifier("adamantite_block"),
+		identifier("adamantite_block"),
 		CustomItems.ADAMANTITE_BLOCK,
+		note = Note.natural(1, Note.Tone.A),
+		instrument = org.bukkit.Instrument.PIANO
+	)
+
+	val COMPUTER_CORE: CustomBlock = CustomBlock(
+		identifier("computer_core"),
+		CustomItems.COMPUTER_CORE,
+		note = Note.natural(1, Note.Tone.B),
+		instrument = org.bukkit.Instrument.PIANO
+	)
+
+	val FUEL_JUNCTION: CustomBlock = CustomBlock(
+		identifier("fuel_junction"),
+		CustomItems.FUEL_JUNCTION,
+		note = Note.natural(1, Note.Tone.C),
+		instrument = org.bukkit.Instrument.PIANO
+	)
+
+	val CAPACITOR: CustomBlock = CustomBlock(
+		identifier("capacitor"),
+		CustomItems.CAPACITOR,
 		note = Note.natural(1, Note.Tone.D),
+		instrument = org.bukkit.Instrument.PIANO
+	)
+
+	val STEEL_FRAME: CustomBlock = CustomBlock(
+		identifier("steel_frame"),
+		CustomItems.STEEL_FRAME,
+		note = Note.natural(1, Note.Tone.E),
 		instrument = org.bukkit.Instrument.PIANO
 	)
 
--- server/src/main/kotlin/net/stellarica/server/material/custom/item/CustomItems.kt
+++ server/src/main/kotlin/net/stellarica/server/material/custom/item/CustomItems.kt
@@ -2,7 +2,7 @@ package net.stellarica.server.material.custom.item
 
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.item.Items
-import net.stellarica.server.StellaricaServer
+import net.stellarica.server.StellaricaServer.Companion.identifier
 import net.stellarica.server.material.custom.block.CustomBlocks
 import net.stellarica.server.material.type.item.ItemType
 
@@ -10,7 +10,7 @@ import net.stellarica.server.material.type.item.ItemType
 object CustomItems {
 
 	val DETECTOR: CustomItem = CustomItem(
-		StellaricaServer.identifier("detector"),
+		identifier("detector"),
 		"Detector",
 		listOf("Detects multiblocks"),
 		ItemType.of(Items.ARROW),
@@ -19,7 +19,7 @@ object CustomItems {
 	)
 
 	val ADAMANTITE_BLOCK: CustomItem = CustomItem(
-		StellaricaServer.identifier("adamantite_block"),
+		identifier("adamantite_block"),
 		"Block of Adamantite",
 		listOf(),
 		ItemType.of(Items.NOTE_BLOCK),
@@ -29,13 +29,69 @@ object CustomItems {
 	)
 
 	val TEST_BLASTER: CustomItem = CustomItem(
-		StellaricaServer.identifier("test_blaster"),
+		identifier("test_blaster"),
 		"<aqua>Blaster",
 		listOf("<dark_gray>pew pew"),
 		ItemType.of(Items.GOLDEN_SHOVEL),
 		2,
 		null,
 		maxPower = 100
+	)
+
+	val COMPUTER_CORE: CustomItem = CustomItem(
+		identifier("computer_core"),
+		"Computer Core",
+		listOf(),
+		ItemType.of(Items.NOTE_BLOCK),
+		3,
+		null,
+		CustomBlocks.COMPUTER_CORE
+	)
+
+	val FUEL_JUNCTION: CustomItem = CustomItem(
+		identifier("fuel_junction"),
+		"Fuel Junction",
+		listOf(),
+		ItemType.of(Items.NOTE_BLOCK),
+		4,
+		null,
+		CustomBlocks.FUEL_JUNCTION
+	)
+
+	val CAPACITOR: CustomItem = CustomItem(
+		identifier("capacitor"),
+		"Capacitor",
+		listOf(),
+		ItemType.of(Items.NOTE_BLOCK),
+		5,
+		null,
+		CustomBlocks.CAPACITOR
+	)
+
+	val STEEL_FRAME: CustomItem = CustomItem(
+		identifier("steel_frame"),
+		"Steel Frame",
+		listOf(),
+		ItemType.of(Items.NOTE_BLOCK),
+		6,
+		null,
+		CustomBlocks.STEEL_FRAME
+	)
+
+	val WIRE: CustomItem = CustomItem(
+		identifier("wire"),
+		"Wire",
+		listOf(),
+		ItemType.of(Items.FLINT),
+		1
+	)
+
+	val PROCESSOR: CustomItem = CustomItem(
+		identifier("processor"),
+		"Processor",
+		listOf(),
+		ItemType.of(Items.FLINT),
+		2
 	)
 
 	fun all(): Set<CustomItem> { // can't do lazy{} because reflection
