da557c94a034fa434e032e487637a0f2c9fe3034
trainb0y <trainb0ydev@gmail.com>
2023-08-02 10:01:52 -0600

    Revert "Delete the cursed reflection piles"
    This reverts commit 10fb270c3477946bb844432555febd78fecadf04.

---
+++ server/src/main/kotlin/net/stellarica/server/multiblock/Multiblocks.kt
@@ -0,0 +1,20 @@
+package net.stellarica.server.multiblock
+
+import net.minecraft.resources.ResourceLocation
+import net.stellarica.server.multiblock.type.MultiblockDef
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.javaGetter
+
+object Multiblocks {
+	val all = MultiblockDef::class.sealedSubclasses.map { def ->
+		def.memberProperties.mapNotNull { prop ->
+			// cursed, cursed, cursed! there are better reflection-y ways to do this
+			// but this works:tm: for now
+			prop.javaGetter!!.invoke(def.objectInstance!!) as? MultiblockType
+		}
+	}.flatten()
+
+	private val idMap = all.associateBy { it.id }
+
+	fun byId(id: ResourceLocation): MultiblockType? = idMap[id]
+}
