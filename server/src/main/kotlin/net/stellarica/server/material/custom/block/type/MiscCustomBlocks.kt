2ac31d901e8e302dbfd1f45ca23d350cf6726ad6
trainb0y <trainb0ydev@gmail.com>
2024-01-13 15:30:41 -0700

    Remove most custom items and blocks

--- server/src/main/kotlin/net/stellarica/server/material/custom/block/type/MiscCustomBlocks.kt
+++ server/src/main/kotlin/net/stellarica/server/material/custom/block/type/MiscCustomBlocks.kt
@@ -7,59 +7,10 @@ import org.bukkit.Instrument
 import org.bukkit.Note
 
 data object MiscCustomBlocks : CustomBlockDef {
-	val ADAMANTITE_BLOCK: CustomBlock = CustomBlock(
-		identifier("adamantite_block"),
-		BlockCustomItems.ADAMANTITE_BLOCK,
-		note = Note.natural(1, Note.Tone.A),
-		instrument = Instrument.PIANO
-	)
-
 	val COMPUTER_CORE: CustomBlock = CustomBlock(
 		identifier("computer_core"),
 		BlockCustomItems.COMPUTER_CORE,
 		note = Note.natural(1, Note.Tone.B),
-		instrument = Instrument.PIANO
-	)
-
-	val FUEL_JUNCTION: CustomBlock = CustomBlock(
-		identifier("fuel_junction"),
-		BlockCustomItems.FUEL_JUNCTION,
-		note = Note.natural(1, Note.Tone.C),
-		instrument = Instrument.PIANO
-	)
-
-	val CAPACITOR: CustomBlock = CustomBlock(
-		identifier("capacitor"),
-		BlockCustomItems.CAPACITOR,
-		note = Note.natural(1, Note.Tone.D),
-		instrument = Instrument.PIANO
-	)
-
-	val STEEL_FRAME: CustomBlock = CustomBlock(
-		identifier("steel_frame"),
-		BlockCustomItems.STEEL_FRAME,
-		note = Note.natural(1, Note.Tone.E),
-		instrument = Instrument.PIANO
-	)
-
-	val ADAMANTITE_ORE: CustomBlock = CustomBlock(
-		identifier("adamantite_ore"),
-		BlockCustomItems.ADAMANTITE_ORE,
-		note = Note.natural(1, Note.Tone.F),
-		instrument = Instrument.PIANO
-	)
-
-	val URANIUM_RAW_BLOCK: CustomBlock = CustomBlock(
-		identifier("uranium_raw_block"),
-		BlockCustomItems.URANIUM_RAW_BLOCK,
-		note = Note.natural(1, Note.Tone.G),
-		instrument = Instrument.PIANO
-	)
-
-	val URANIUM_ORE: CustomBlock = CustomBlock(
-		identifier("uranium_ore"),
-		BlockCustomItems.URANIUM_ORE,
-		note = Note.natural(0, Note.Tone.A),
 		instrument = Instrument.PIANO
 	)
 }
--- server/src/main/kotlin/net/stellarica/server/material/custom/item/type/BlockCustomItems.kt
+++ server/src/main/kotlin/net/stellarica/server/material/custom/item/type/BlockCustomItems.kt
@@ -7,16 +7,6 @@ import net.stellarica.server.material.custom.item.CustomItem
 import net.stellarica.server.material.type.item.ItemType
 
 data object BlockCustomItems : CustomItemDef {
-	val ADAMANTITE_BLOCK: CustomItem = CustomItem(
-		identifier("adamantite_block"),
-		"Block of Adamantite",
-		listOf(),
-		ItemType.of(Items.NOTE_BLOCK),
-		2,
-		null,
-		MiscCustomBlocks.ADAMANTITE_BLOCK
-	)
-
 	val COMPUTER_CORE: CustomItem = CustomItem(
 		identifier("computer_core"),
 		"Computer Core",
@@ -25,65 +15,5 @@ data object BlockCustomItems : CustomItemDef {
 		3,
 		null,
 		MiscCustomBlocks.COMPUTER_CORE
-	)
-
-	val FUEL_JUNCTION: CustomItem = CustomItem(
-		identifier("fuel_junction"),
-		"Fuel Junction",
-		listOf(),
-		ItemType.of(Items.NOTE_BLOCK),
-		4,
-		null,
-		MiscCustomBlocks.FUEL_JUNCTION
-	)
-
-	val CAPACITOR: CustomItem = CustomItem(
-		identifier("capacitor"),
-		"Capacitor",
-		listOf(),
-		ItemType.of(Items.NOTE_BLOCK),
-		5,
-		null,
-		MiscCustomBlocks.CAPACITOR
-	)
-
-	val STEEL_FRAME: CustomItem = CustomItem(
-		identifier("steel_frame"),
-		"Steel Frame",
-		listOf(),
-		ItemType.of(Items.NOTE_BLOCK),
-		6,
-		null,
-		MiscCustomBlocks.STEEL_FRAME
-	)
-
-	val URANIUM_ORE: CustomItem = CustomItem(
-		identifier("uranium_ore"),
-		"Uranium Ore",
-		listOf(),
-		ItemType.of(Items.NOTE_BLOCK),
-		7,
-		null,
-		MiscCustomBlocks.URANIUM_ORE
-	)
-
-	val URANIUM_RAW_BLOCK: CustomItem = CustomItem(
-		identifier("uranium_raw_block"),
-		"Raw Uranium Block",
-		listOf(),
-		ItemType.of(Items.NOTE_BLOCK),
-		8,
-		null,
-		MiscCustomBlocks.URANIUM_RAW_BLOCK
-	)
-
-	val ADAMANTITE_ORE: CustomItem = CustomItem(
-		identifier("adamantite_ore"),
-		"Adamantite Ore",
-		listOf(),
-		ItemType.of(Items.NOTE_BLOCK),
-		9,
-		null,
-		MiscCustomBlocks.ADAMANTITE_ORE
 	)
 }
