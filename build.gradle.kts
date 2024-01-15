e8ab62d21ae120c51b77373ef6baf10c90e9b7b5
trainb0y <trainb0ydev@gmail.com>
2024-01-14 19:10:38 -0700

    Detekt is cringe

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
 	java
 	kotlin("jvm")
-	id("io.gitlab.arturbosch.detekt")
 	id("fabric-loom") apply false
 }
 
@@ -13,21 +12,10 @@ allprojects {
 	apply {
 		plugin("java")
 		plugin("kotlin")
-		plugin("io.gitlab.arturbosch.detekt")
 	}
 	repositories {
 		mavenCentral()
 		mavenLocal()
 		maven("https://jitpack.io")
 	}
-	tasks.getByName("check") {
-		this.setDependsOn(this.dependsOn.filterNot {
-			it is TaskProvider<*> && it.name.contains("detekt")
-		})
-	}
-}
-
-detekt {
-	config = files("config/detekt/detekt.yml")
-	buildUponDefaultConfig = true
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,12 +17,10 @@ pluginManagement {
 		val loom_version: String by settings
 		val kotlin_version: String by settings
 		val shadow_version: String by settings
-		val detekt_version: String by settings
 
 		id("fabric-loom") version loom_version
 		id("io.papermc.paperweight.userdev") version paperweight_version
 		id("com.github.johnrengelman.shadow") version shadow_version
-		id("io.gitlab.arturbosch.detekt") version detekt_version
 		kotlin("jvm") version kotlin_version
 		kotlin("plugin.serialization") version kotlin_version
 	}
